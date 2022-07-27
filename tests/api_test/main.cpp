5b6de887379eacaf626e0ec4c35da8cd2e0135ea
gunderwood <gunderwood@anyarinc.com>
2022-07-27 08:12:57 -0500

    Converted api test to boost ut

--- tests/api_test/main.cpp
+++ tests/api_test/main.cpp
@@ -1,13 +1,14 @@
 // Glaze Library
 // For the license information refer to glaze.hpp
 
-#include "glaze/api/impl.hpp"
+#include <iostream>
+#include <tuple>
 
+#include "boost/ut.hpp"
+#include "glaze/api/impl.hpp"
 #include "glaze/api/std/deque.hpp"
 #include "glaze/api/std/unordered_set.hpp"
 #include "glaze/api/std/span.hpp"
-#include <iostream>
-#include <tuple>
 
 struct my_api
 {
@@ -41,38 +42,100 @@ std::shared_ptr<glaze::api> glaze::create_api(const std::string_view) noexcept
 
 void tests()
 {
+   using namespace boost::ut;
+
    auto io = glaze::create_api();
    
-   try {
-      std::cout << glaze::name<bool> << '\n';
-      std::cout << glaze::name<bool&> << '\n';
-      std::cout << glaze::name<const bool&> << '\n';
-      std::cout << glaze::name<bool*> << '\n';
-      std::cout << glaze::name<const bool*> << '\n';
-      std::cout << glaze::name<std::vector<std::vector<int>*>> << '\n';
-      std::cout << glaze::name<std::unordered_set<std::vector<std::string>>> << '\n';
-      std::cout << glaze::name<double*> << '\n';
-      std::cout << glaze::name<std::vector<float>> << '\n';
-      std::cout << glaze::name<std::deque<bool>> << '\n';
-      std::cout << glaze::name<std::unordered_map<uint64_t, std::string_view>> << '\n';
-      std::cout << glaze::name<const double&> << '\n';
-      std::cout << glaze::name<std::span<double>> << '\n';
-      
-      std::cout << io->get<int>("/x") << '\n';
-      std::cout << io->get<double>("/y") << '\n';
-      auto& v = io->get<std::vector<double>>("/z");
-      std::cout << '{' << v[0] << ',' << v[1] << "}\n";
-      
-      std::cout << glaze::name<std::function<double(const int&, const double&)>> << '\n';
-      
+   "bool type name"_test = [] {
+      {
+         std::string_view b = glaze::name<bool>;
+         expect(b == "bool");
+      }
+      {
+         std::string_view b = glaze::name<bool&>;
+         expect(b == "bool&");
+      }
+      {
+         std::string_view b = glaze::name<const bool&>;
+         expect(b == "const bool&");
+      }
+      {
+         std::string_view b = glaze::name<bool*>;
+         expect(b == "bool*");
+      }
+      {
+         std::string_view b = glaze::name<const bool*>;
+         expect(b == "const bool*");
+      }
+   };
+
+   "vector type name"_test = [] {
+      {
+         std::string_view v =
+            glaze::name<std::vector<std::vector<int>*>>;
+         expect(v == "std::vector<std::vector<int32_t>*>");
+      }
+      {
+         std::string_view v = glaze::name<std::vector<float>>;
+         expect(v == "std::vector<float>");
+      }
+   };
+
+   "unordered type name"_test = [] {
+      {
+         std::string_view u =
+            glaze::name<std::unordered_set<std::vector<std::string>>>;
+         expect(u == "std::unordered_set<std::vector<std::string>>");
+      }
+      {
+         std::string_view u =
+            glaze::name<std::unordered_map<uint64_t, std::string_view>>;
+         expect(u == "std::unordered_map<uint64_t,std::string_view>");
+      }
+   };
+
+   "double type name"_test = [] {
+      {
+         std::string_view d = glaze::name<double*>;
+         expect(d == "double*");
+      }
+      {
+         std::string_view d = glaze::name<const double&>;
+         expect(d == "const double&");
+      }
+   };
+
+   "deque type name"_test = [] {
+      std::string_view d = glaze::name<std::deque<bool>>;
+      expect(d == "std::vector<bool>");
+   };
+
+   "span type name"_test = [] {
+      std::string_view s = glaze::name<std::span<double>>;
+      expect(s == "std::span<double,18446744073709551615>");
+   };
+
+   "my_api type io"_test = [&] {
+      auto& x = io->get<int>("/x");
+      auto& y = io->get<double>("/y");
+      auto& z = io->get<std::vector<double>>("/z");
+      expect(x == 7);
+      expect(y == 5.5);
+      expect(z == std::vector<double>{1.0,2.0});
+   };
+
+   "function type name"_test = [] {
+      std::string_view f =
+         glaze::name<std::function<double(const int&, const double&)>>;
+      expect(f == "std::function<double(const int32_t&,const double&)");
+   };
+
+   "function type io"_test = [&] {
       int x = 7;
       double y = 5.5;
       auto& f = io->get<std::function<double(const int&, const double&)>>("/f");
-      std::cout << f(x, y) << '\n';
-   }
-   catch (const std::exception& e) {
-      std::cout << e.what() << '\n';
-   }
+      expect(f(x, y) == 38.5);
+   };
 }
 
 int main()
