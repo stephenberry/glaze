ae4682e56427802af3db766eb1ec3bbfcdd43503
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-01 11:45:43 -0500

    Update read.hpp

--- include/glaze/json/read.hpp
+++ include/glaze/json/read.hpp
@@ -60,7 +60,7 @@ namespace glaze
       struct read<json>
       {
          template <class T, class It0, class It1>
-         static void op(T& value, It0&& it, It1&& end) {
+         static void op(T&& value, It0&& it, It1&& end) {
             from_json<std::decay_t<T>>::op(value, std::forward<It0>(it), std::forward<It1>(end));
          }
       };
@@ -68,7 +68,7 @@ namespace glaze
       template <bool_t T>
       struct from_json<T>
       {
-         static void op(bool_t auto& value, auto&& it, auto&& end)
+         static void op(bool_t auto&& value, auto&& it, auto&& end)
          {
             skip_ws(it, end);
             if (it < end) [[likely]] {
