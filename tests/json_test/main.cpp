84f25bc7864c2d8a1590c9cad8ae32763f5bbc75
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-16 08:38:24 -0500

    merged write_test into json_tests

--- tests/json_test/main.cpp
+++ tests/json_test/main.cpp
@@ -1171,6 +1171,375 @@ void Read_tests() {
    };
 }
 
+using Geodetic = v3;
+
+struct ThreeODetic
+{
+   Geodetic g1{};
+   int x1;
+};
+
+struct NineODetic
+{
+   ThreeODetic t1{};
+   Geodetic g1;
+};
+
+struct Named
+{
+   std::string name;
+   NineODetic value;
+};
+
+template <>
+struct glaze::meta<ThreeODetic>
+{
+   using t = ThreeODetic;
+   static constexpr auto value = glaze::array("geo", &t::g1, "int", &t::x1);
+};
+
+template <>
+struct glaze::meta<NineODetic>
+{
+   using n = NineODetic;
+   static constexpr auto value = glaze::array(&n::t1, &n::g1);
+};
+
+template <>
+struct glaze::meta<Named>
+{
+   using n = Named;
+   static constexpr auto value =
+      glaze::object("name", &n::name, "value", &n::value);
+};
+
+struct EmptyArray
+{};
+
+template <>
+struct glaze::meta<EmptyArray>
+{
+   static constexpr auto value = glaze::array();
+};
+
+struct EmptyObject
+{};
+
+//* Empty object not allowed
+template <>
+struct glaze::meta<EmptyObject>
+{
+   static constexpr auto value = glaze::object();
+};
+
+void Write_tests() {
+   using namespace boost::ut;
+
+   "Write floating point types"_test = [] {
+      {
+         std::string s;
+         float f{0.96875f};
+         glaze::write_json(f, s);
+         expect(s == "0.96875");
+      }
+      {
+         std::string s;
+         double f{0.96875};
+         glaze::write_json(f, s);
+         expect(s == "0.96875");
+      }
+      {
+         std::string s;
+         long double f{0.96875L};
+         glaze::write_json(f, s);
+         expect(s == "0.96875");
+      }
+   };
+
+   "Write integral types"_test = [] {
+      {
+         std::string s;
+         bool v{1};
+         glaze::write_json(v, s);
+         expect(s == "true");
+      }
+//* Two test below fail, unless changed to the following:
+      {
+         std::string s;
+         char v{'a'};
+         glaze::write_json(v, s);
+         //Is this what we want instead?
+         expect(s == R"("a")");  // std::to_string(static_cast<int>('a')));
+      }
+      {
+         std::string s;
+         wchar_t v{'a'};
+         glaze::write_json(v, s); //This line gives warning about converting wchar to char, is that fine? Should we write a to_buffer template to handle type wchar?
+         // Is the below what we actually expect?
+         expect(s == R"("a")");  // std::to_string(static_cast<int>('a')));
+      }
+      {
+         std::string s;
+         short v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         int v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         long v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         long long v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         unsigned short v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         unsigned int v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         unsigned long v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+      {
+         std::string s;
+         unsigned long long v{1};
+         glaze::write_json(v, s);
+         expect(s == "1");
+      }
+   };
+
+//* write_json cannot handle type variant by itself, need to use std::get. Do we need to add variant support?
+   "Write variant"_test = [] {
+      std::variant<int, double, Geodetic> var;
+
+      var = 1;
+      auto i = std::get<0>(var);
+      std::string ibuf;
+      glaze::write_json(i, ibuf);
+      expect(ibuf == R"(1)");
+
+      var = 2.2;
+      auto d = std::get<1>(var);
+      std::string dbuf;
+      glaze::write_json(d, dbuf);
+      expect(dbuf == R"(2.2)");
+
+      var = Geodetic{1.0, 2.0, 5.0};
+      auto g = std::get<2>(var);
+      std::string gbuf;
+      glaze::write_json(g, gbuf);
+      expect(gbuf == R"([1,2,5])");
+   };
+
+   "Write empty array structure"_test = [] {
+      EmptyArray e;
+      std::string buf;
+      glaze::write_json(e, buf);
+      expect(buf == R"([])");
+   };
+
+//* Empty object not allowed
+   "Write empty object structure"_test = [] {
+      EmptyObject e;
+      std::string buf;
+      glaze::write_json(e, buf);
+      //expect(buf == R"({})");
+   };
+
+   "Write c-string"_test = [] {
+      std::string s = "aasdf";
+      char *c = s.data();
+      std::string buf;
+      glaze::write_json(c, buf);
+      expect(buf == R"("aasdf")");
+   };
+
+   "Write constant double"_test = [] {
+      const double d = 6.125;
+      std::string buf;
+      glaze::write_json(d, buf);
+      expect(buf == R"(6.125)");
+   };
+
+   "Write constant bool"_test = [] {
+      const bool b = true;
+      ;
+      std::string buf;
+      glaze::write_json(b, buf);
+      expect(buf == R"(true)");
+   };
+
+   "Write constant int"_test = [] {
+      const int i = 505;
+      std::string buf;
+      glaze::write_json(i, buf);
+      expect(buf == R"(505)");
+   };
+
+   "Write vector"_test = [] {
+      {
+         std::vector<double> v{1.1, 2.2, 3.3, 4.4};
+         std::string s;
+         glaze::write_json(v, s);
+
+         expect(s == "[1.1,2.2,3.3,4.4]");
+      }
+      {
+         std::vector<bool> v{true, false, true, false};
+         std::string s;
+         glaze::write_json(v, s);
+
+         expect(s == "[true,false,true,false]");
+      }
+   };
+
+   "Write list"_test = [] {
+      std::string in, inr = "[1,2,3,4]";
+      std::list<int> l{1, 2, 3, 4};
+      glaze::write_json(l, in);
+
+      expect(in == inr);
+   };
+
+   "Write forward list"_test = [] {
+      std::string in, inr = "[1,2,3,4]";
+      std::forward_list<int> l{1, 2, 3, 4};
+      glaze::write_json(l, in);
+
+      expect(in == inr);
+   };
+
+   "Write deque"_test = [] {
+      std::string in, inr = "[1,2,3,4]";
+      std::deque<int> l{1, 2, 3, 4};
+      glaze::write_json(l, in);
+
+      expect(in == inr);
+   };
+
+   "Write array"_test = [] {
+      std::array<double, 4> v{1.1, 2.2, 3.3, 4.4};
+      std::string s;
+      glaze::write_json(v, s);
+
+      expect(s == "[1.1,2.2,3.3,4.4]");
+   };
+
+   "Write map"_test = [] {
+      std::map<std::string, double> m{{"a", 2.2}, {"b", 11.111}, {"c", 211.2}};
+      std::string s;
+      glaze::write_json(m, s);
+
+      expect(s == R"({"a":2.2,"b":11.111,"c":211.2})");
+   };
+
+   "Write integer map"_test = [] {
+      std::map<int, double> m{{3, 2.2}, {5, 211.2}, {7, 11.111}};
+      std::string s;
+      glaze::write_json(m, s);
+
+      expect(s == R"({"3":2.2,"5":211.2,"7":11.111})");
+   };
+
+//* Gives 23 errors. Errors come from an MSVC include file "utility": it claims that the base class is undifined.
+   "Write object"_test = [] {
+      Named n{"Hello, world!", {{{21, 15, 13}, 0}, {0}}};
+
+      std::string s;
+      s.reserve(1000);
+      [[maybe_unused]] auto i = std::back_inserter(s);
+      //glaze::write_json(n, s);
+
+      //expect(
+         //s ==
+         //R"({"name":"Hello, world!","value":[{"geo":[21,15,13],"int":0},[0,0,0]]})");
+   };
+
+   "Write boolean"_test = [] {
+      {
+         bool b = true;
+         std::string s;
+         glaze::write_json(b, s);
+
+         expect(s == R"(true)");
+      }
+      {
+         bool b = false;
+         std::string s;
+         glaze::write_json(b, s);
+
+         expect(s == R"(false)");
+      }
+   };
+
+   "Hello World"_test = [] {
+      std::unordered_map<std::string, std::string> m;
+      m["Hello"] = "World";
+      std::string buf;
+      glaze::write_json(m, buf);
+
+      expect(buf == R"({"Hello":"World"})");
+   };
+
+   "Number"_test = [] {
+      std::unordered_map<std::string, double> x;
+      x["number"] = 5.55;
+
+      std::string jx;
+      glaze::write_json(x, jx);
+
+      expect(jx == R"({"number":5.55})");
+   };
+
+   "Nested array"_test = [] {
+      std::vector<Geodetic> v(2);
+      std::string buf;
+
+      glaze::write_json(v, buf);
+      expect(buf == R"([[0,0,0],[0,0,0]])");
+   };
+
+   "Nested map"_test = [] {
+      std::map<std::string, Geodetic> m;
+      m["1"];
+      m["2"];
+      std::string buf;
+
+      glaze::write_json(m, buf);
+      expect(buf == R"({"1":[0,0,0],"2":[0,0,0]})");
+   };
+
+   "Nested map 2"_test = [] {
+      std::map<std::string, std::vector<double>> m;
+      m["1"] = {4.0, 0.0, 0.0};
+      m["2"] = {5.0, 0.0, 0.0, 4.0};
+      std::string buf;
+
+      glaze::write_json(m, buf);
+      expect(buf == R"({"1":[4,0,0],"2":[5,0,0,4]})");
+   };
+}
+
 int main()
 {
    using namespace boost::ut;
@@ -1190,4 +1559,5 @@ int main()
    early_end(); 
    bench();
    Read_tests();
+   Write_tests();
 }
