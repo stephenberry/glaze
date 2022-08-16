083d60b5cff60d7222174fb6374094001a35f046
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-16 10:28:44 -0500

    Added macros header to reduce redundancy

--- include/glaze/thread/threadpool.hpp
+++ include/glaze/thread/threadpool.hpp
@@ -12,15 +12,7 @@
 #include <thread>
 #include <vector>
 
-#ifdef _WIN32
-#ifdef NOMINMAX
-#include <windows.h>
-#else
-#define NOMINMAX
-#include <windows.h>
-#undef NOMINMAX
-#endif
-#endif
+#include "glaze/util/macros.hpp"
 
 namespace glaze
 {
