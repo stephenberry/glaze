1d1b48956a827533582b7b6644be5393375dd652
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-18 09:54:13 -0500

    recorder cleanup

--- include/glaze/record/recorder.hpp
+++ include/glaze/record/recorder.hpp
@@ -9,47 +9,54 @@
 
 namespace glaze
 {
-   template <class... T>
-   auto to_deque(std::variant<T...>&&)
+   namespace detail
    {
-      return std::variant<std::monostate, std::deque<T>...>{};
-   }
-
-   template <class... T>
-   auto to_variant_pointer(std::variant<T...>&&)
-   {
-      return std::variant<T*...>{};
-   }
-   
-   template <class Data>
-   struct assigner
-   {
-      assigner(Data& data) : data(data) {}
-      assigner(const assigner&) = default;
-      assigner(assigner&&) = default;
-      assigner& operator=(const assigner&) = default;
-      assigner& operator=(assigner&&) = default;
-      
-      Data& data;
-      
-      template <class T>
-      void operator=(T& ref) {
-         data = std::make_pair(typename Data::first_type{std::deque<T>{}}, &ref);
+      template <class... T>
+      auto to_deque(std::variant<T...>&&)
+      {
+         return std::variant<std::monostate, std::deque<T>...>{};
       }
-   };
 
+      template <class... T>
+      auto to_variant_pointer(std::variant<T...>&&)
+      {
+         return std::variant<T*...>{};
+      }
+      
+      template <class Data>
+      struct assigner
+      {
+         assigner(Data& data) : data(data) {}
+         assigner(const assigner&) = default;
+         assigner(assigner&&) = default;
+         assigner& operator=(const assigner&) = default;
+         assigner& operator=(assigner&&) = default;
+         
+         Data& data;
+         
+         template <class T>
+         void operator=(T& ref) {
+            data = std::make_pair(typename Data::first_type{std::deque<T>{}}, &ref);
+         }
+      };
+   }
+
+   /// <summary>
+   /// recorder for saving state over the course of a run
+   /// deques are used to avoid reallocation for large amounts of data as the recording length is typically unknown
+   /// </summary>
    template <is_variant variant_t>
    struct recorder
    {
-      using variant_p = decltype(to_variant_pointer(std::declval<variant_t>()));
-      using container_type = decltype(to_deque(std::declval<variant_t>()));
+      using variant_p = decltype(detail::to_variant_pointer(std::declval<variant_t>()));
+      using container_type = decltype(detail::to_deque(std::declval<variant_t>()));
 
       std::deque<std::pair<std::string, std::pair<container_type, void*>>>
          data;
       
       auto operator[](const std::string_view name) {
          auto& d = data.emplace_back(name, std::make_pair(std::monostate{}, nullptr));
-         return assigner<std::pair<container_type, void*>>{ d.second };
+         return detail::assigner<std::pair<container_type, void*>>{ d.second };
       }
 
       void update()
