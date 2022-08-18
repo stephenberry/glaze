bf0c1ddb9ebf16e069dea4427d59953b591c489b
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-18 09:30:28 -0500

    added helper includes

---
+++ include/glaze/binary.hpp
@@ -0,0 +1,8 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#pragma once
+
+#include "glaze/binary/header.hpp"
+#include "glaze/binary/read.hpp"
+#include "glaze/binary/write.hpp"
---
+++ include/glaze/json.hpp
@@ -0,0 +1,10 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#pragma once
+
+#include "glaze/json/read.hpp"
+#include "glaze/json/write.hpp"
+#include "glaze/json/json_ptr.hpp"
+#include "glaze/json/overwrite.hpp"
+#include "glaze/json/prettify.hpp"
