680eadd6ed398e2d53e6161c7ae0f7a379229da6
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-23 10:49:37 -0500

    Create bit.hpp

---
+++ include/glaze/util/bit.hpp
@@ -0,0 +1,27 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#pragma once
+
+#include <utility>
+
+namespace glaze
+{
+   template <class To, class From>
+   std::enable_if_t<
+       sizeof(To) == sizeof(From) &&
+       std::is_trivially_copyable_v<From> &&
+       std::is_trivially_copyable_v<To>,
+       To>
+   // constexpr support needs compiler magic
+   bit_cast(const From& src) noexcept
+   {
+       static_assert(std::is_trivially_constructible_v<To>,
+           "This implementation additionally requires "
+           "destination type to be trivially constructible");
+    
+       To dst;
+       std::memcpy(&dst, &src, sizeof(To));
+       return dst;
+   }
+}
