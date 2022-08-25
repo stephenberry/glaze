9f7804c5d9c9404d4a268d80987f47cf02694828
Stephen Berry <stephenberry.developer@gmail.com>
2022-08-25 10:09:48 -0500

    cleaning | using enum general_state

--- include/glaze/binary/header.hpp
+++ include/glaze/binary/header.hpp
@@ -7,25 +7,25 @@
 
 namespace glaze::detail
 {
-   struct header8 {
+   struct header8 final {
       uint8_t config : 2;
       uint8_t size : 6;
    };
    static_assert(sizeof(header8) == 1);
    
-   struct header16 {
+   struct header16 final {
       uint16_t config : 2;
       uint16_t size : 14;
    };
    static_assert(sizeof(header16) == 2);
    
-   struct header32 {
+   struct header32 final {
       uint32_t config : 2;
       uint32_t size : 30;
    };
    static_assert(sizeof(header32) == 4);
    
-   struct header64 {
+   struct header64 final {
       uint64_t config : 2;
       uint64_t size : 62;
    };
