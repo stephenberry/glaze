542b357fba2d9733aacfe0964deda448b049dde6
Stephen Berry <stephenberry.developer@gmail.com>
2022-10-25 13:05:43 -0500

    MSVC preprocessor flag

--- include/glaze/core/macros.hpp
+++ include/glaze/core/macros.hpp
@@ -15,10 +15,10 @@
 #define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
 #define EXPAND1(...) __VA_ARGS__
 
-#define FOR_EACH(macro, ...)                                    \
+#define FOR_EACH(macro, ...) \
   __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
-#define FOR_EACH_HELPER(macro, a1, ...)                         \
-  macro(a1)__VA_OPT__(,)                                                     \
+#define FOR_EACH_HELPER(macro, a, ...) \
+  macro(a)__VA_OPT__(,) \
   __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
 #define FOR_EACH_AGAIN() FOR_EACH_HELPER
 
