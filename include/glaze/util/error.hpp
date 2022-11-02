7be91e60a2eef0f21dfb0175b14fd8586b2a9cdb
Stephen Berry <stephenberry.developer@gmail.com>
2022-11-02 06:13:58 -0500

    Added error.hpp and more noexcept support

---
+++ include/glaze/util/error.hpp
@@ -0,0 +1,15 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#pragma once
+
+namespace glz
+{
+   enum class error : uint32_t
+   {
+      none,
+      read,
+      write,
+      maximum_size_exceeded,
+   };
+}
