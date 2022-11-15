551c546a0cacfd7e2081a9e8fb4bc0e0b2499c4b
Stephen Berry <stephenberry.developer@gmail.com>
2022-11-15 14:16:08 -0600

    removed ix from context

--- include/glaze/core/context.hpp
+++ include/glaze/core/context.hpp
@@ -8,9 +8,7 @@ namespace glz
    // Runtime context for configuration
    // We do not template the context on iterators so that it can be easily shared across buffer implementations
    struct context final
-   {
-      size_t ix{}; // index for buffer writing
-      
+   {      
       bool prettify = false; // write out prettified JSON
    };
    
