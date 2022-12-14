74ead7e8a76823f9522c366e78b95f3d90cfd404
Stephen Berry <stephenberry.developer@gmail.com>
2022-12-14 10:08:57 -0600

    removed to_variant_pointer
    unused and expensive for compile time

--- include/glaze/util/variant.hpp
+++ include/glaze/util/variant.hpp
@@ -12,12 +12,6 @@ namespace glz
    template <class T>
    concept is_variant = is_specialization_v<T, std::variant>;
    
-   template <class... T>
-   auto to_variant_pointer(std::variant<T...>&&)
-   {
-      return std::variant<T*...>{};
-   }
-   
    template <class ...T>
    size_t variant_container_size(const std::variant<T...>& v)
    {
