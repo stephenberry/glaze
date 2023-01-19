86ed188ac11e49f03244e59acdf65622ecf10cf4
Stephen Berry <stephenberry.developer@gmail.com>
2023-01-19 09:03:00 -0600

    custom type handling test

--- tests/json_test/json_test.cpp
+++ tests/json_test/json_test.cpp
@@ -2952,6 +2952,61 @@ suite json_schema = [] {
 };
 
 
+// custom type handling
+struct date
+{
+   uint64_t data;
+   std::string human_readable;
+};
+
+template <>
+struct glz::meta<date>
+{
+   using T = date;
+   static constexpr auto value = object("date", &T::human_readable);
+};
+
+namespace glz::detail
+{
+   template <>
+   struct from_json<date>
+   {
+      template <auto Opts>
+      static void op(date& value, auto&&... args)
+      {
+         read<json>::op<Opts>(value.human_readable, args...);
+         value.data = std::stoi(value.human_readable);
+      }
+   };
+
+   template <>
+   struct to_json<date>
+   {
+      template <auto Opts>
+      static void op(date& value, auto&&... args) noexcept
+      {
+         value.human_readable = std::to_string(value.data);
+         write<json>::op<Opts>(value.human_readable, args...);
+      }
+   };
+}
+
+suite date_test = []
+{
+   "date"_test = [] {
+      date d{};
+      d.data = 55;
+      
+      std::string s{};
+      glz::write_json(d, s);
+      
+      expect(s == R"("55")");
+      
+      d.data = 0;
+      glz::read_json(d, s);
+      expect(d.data == 55);
+   };
+};
 
 int main()
 {
