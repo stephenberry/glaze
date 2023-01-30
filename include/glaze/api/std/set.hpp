d0fc9d6a4068c5860f6508f0466a92daf2e2cfa4
Stephen Berry <stephenberry.developer@gmail.com>
2023-01-30 13:28:10 -0600

    added set.hpp

---
+++ include/glaze/api/std/set.hpp
@@ -0,0 +1,22 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#pragma once
+
+#include <set>
+
+#include "glaze/api/name.hpp"
+
+namespace glz
+{
+   template <class T>
+   concept set = is_specialization_v<T, std::set>;
+   
+   template <set T>
+   struct meta<T> {
+      using V = typename T::value_type;
+      static constexpr std::string_view name = detail::join_v<chars<"std::set<">,
+      name_v<V>,
+      chars<">">>;
+   };
+}
