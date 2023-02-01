7753e47a89f9304f3f5cfb49eb028a0dafb913a9
Stephen Berry <stephenberry.developer@gmail.com>
2023-02-01 14:02:56 -0600

    msvc ice fix?

--- include/glaze/json/json_ptr.hpp
+++ include/glaze/json/json_ptr.hpp
@@ -588,9 +588,10 @@ namespace glz
          std::span<std::remove_reference_t<decltype(*it)>> ret;
          
          for_each<N>([&](auto I) {
-            static constexpr auto key = [] {
+            using index_t = decltype(I);
+            static constexpr auto key = [](index_t Index) {
                return std::get<decltype(I)::value>(tokens);
-            }(); // MSVC internal compiler error workaround
+            }({}); // MSVC internal compiler error workaround
             if constexpr (maybe_numeric_key(key)) {
                switch (*it)
                {
