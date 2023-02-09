e929886d1c829e3eb3f2215c91963200b8174683
Stephen Berry <stephenberry.developer@gmail.com>
2023-02-09 12:39:07 -0600

    removed variant_container_size

--- include/glaze/util/variant.hpp
+++ include/glaze/util/variant.hpp
@@ -11,18 +11,4 @@ namespace glz
 {
    template <class T>
    concept is_variant = is_specialization_v<T, std::variant>;
-   
-   template <class ...T>
-   size_t variant_container_size(const std::variant<T...>& v)
-   {
-       return std::visit([](auto&& x) -> size_t {
-          using Container = std::decay_t<decltype(x)>;
-          if constexpr (std::same_as<Container, std::monostate>) {
-             throw std::runtime_error("container_size: container is monostate");
-          }
-          else {
-             return x.size();
-          }
-       }, v);
-   }
 }
