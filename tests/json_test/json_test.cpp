c461af33db4461bd797c56375aed3a02327f6082
Michael Walcott <mwalcott@anyarinc.com>
2023-02-24 10:24:55 -0600

    Move all tests in json_test.cpp to suites. This is related to when tls destructors are called in gcc. So either all test need to be suites or none of them should be to prevent use after free when using thread_local vars.

--- tests/json_test/json_test.cpp
+++ tests/json_test/json_test.cpp
@@ -326,7 +326,7 @@ suite escaping_tests = [] {
    };
 };
 
-void basic_types() {
+suite basic_types = [] {
    using namespace boost::ut;
 
    "double write"_test = [] {
@@ -402,7 +402,7 @@ void basic_types() {
       expect(glz::read_json(val, "false") == glz::error_code::none);
       expect(val == false);
    };
-   
+
    "bool read invalid"_test = [] {
       bool val{};
       expect(glz::read_json(val, "tru") != glz::error_code::none);
@@ -425,9 +425,9 @@ void basic_types() {
       expect(glz::read_json(val, "\"as\\\"df\\\\ghjkl\"") == glz::error_code::none);
       expect(val == "as\"df\\ghjkl");
    };
-}
+};
 
-void container_types() {
+suite container_types = [] {
    using namespace boost::ut;
    "vector int roundtrip"_test = [] {
       std::vector<int> vec(100);
@@ -556,9 +556,9 @@ void container_types() {
       expect(glz::read_json(pair2, buffer) == glz::error_code::none);
       expect(pair == pair2);
    };
-}
+};
 
-void nullable_types() {
+suite nullable_types = [] {
    using namespace boost::ut;
    "optional"_test = [] {
       std::optional<int> oint{};
@@ -614,10 +614,9 @@ void nullable_types() {
       glz::write_json(ptr, buffer);
       expect(buffer == "null");
    };
-}
+};
 
-void enum_types()
-{
+suite enum_types = [] {
    using namespace boost::ut;
    "enum"_test = [] {
       Color color = Color::Red;
@@ -631,9 +630,9 @@ void enum_types()
       glz::write_json(color, buffer);
       expect(buffer == "\"Green\"");
    };
-}
+};
 
-void user_types() {
+suite user_types = [] {
    using namespace boost::ut;
 
    "user array"_test = [] {
@@ -838,25 +837,23 @@ void user_types() {
          glz::name_v<glz::detail::member_tuple_t<Thing>> == "glz::tuplet::tuple<sub_thing,std::array<sub_thing2,1>,V3,std::list<int32_t>,std::deque<double>,std::vector<V3>,int32_t,double,bool,char,std::variant<var1_t,var2_t>,Color,std::vector<bool>,std::shared_ptr<sub_thing>,std::optional<V3>,std::array<std::string,4>,std::map<std::string,int32_t>,std::map<int32_t,double>,sub_thing*>"
       );
    };
-}
+};
 
-void json_pointer() {
+suite json_pointer = [] {
    using namespace boost::ut;
 
    "seek"_test = [] {
       Thing thing{};
       std::any a{};
       glz::seek([&](auto&& val) { a = val; }, thing, "/thing_ptr/a");
-      expect(a.has_value() &&
-             std::any_cast<double>(a) == thing.thing_ptr->a);
+      expect(a.has_value() && std::any_cast<double>(a) == thing.thing_ptr->a);
    };
 
    "seek lambda"_test = [] {
       Thing thing{};
       std::any b{};
       glz::seek([&](auto&& val) { b = val; }, thing, "/thing/b");
-      expect(b.has_value() &&
-             std::any_cast<std::string>(b) == thing.thing.b);
+      expect(b.has_value() && std::any_cast<std::string>(b) == thing.thing.b);
    };
 
    "get"_test = [] {
@@ -868,7 +865,7 @@ void json_pointer() {
       expect(thing.vector[1].x == glz::get<double>(thing, "/vector/1/0"));
       expect(thing.thing_ptr == glz::get<sub_thing*>(thing, "/thing_ptr"));
 
-      //Invalid lookup
+      // Invalid lookup
       expect(glz::get<char>(thing, "/thing_ptr/a").has_value() == false);
       expect(glz::get_if<char>(thing, "/thing_ptr/a") == nullptr);
       expect(glz::get<double>(thing, "/thing_ptr/c").has_value() == false);
@@ -906,15 +903,14 @@ void json_pointer() {
    "read_as_json"_test = [] {
       Thing thing{};
       glz::read_as_json(thing, "/vec3", "[7.6, 1292.1, 0.333]");
-      expect(thing.vec3.x == 7.6 && thing.vec3.y == 1292.1 &&
-             thing.vec3.z == 0.333);
+      expect(thing.vec3.x == 7.6 && thing.vec3.y == 1292.1 && thing.vec3.z == 0.333);
 
       glz::read_as_json(thing, "/vec3/2", "999.9");
       expect(thing.vec3.z == 999.9);
    };
 
    "valid"_test = [] {
-      [[maybe_unused]] constexpr bool is_valid = glz::valid<Thing, "/thing/a", double>(); //Verify constexpr
+      [[maybe_unused]] constexpr bool is_valid = glz::valid<Thing, "/thing/a", double>();  // Verify constexpr
 
       expect(glz::valid<Thing, "/thing_ptr/a", double>() == true);
       expect(glz::valid<Thing, "/thing_ptr/a", int>() == false);
@@ -929,10 +925,9 @@ void json_pointer() {
       expect(glz::valid<Thing, "/vector/1", V3>() == true);
       expect(glz::valid<Thing, "/vector/1/0", double>() == true);
    };
-}
+};
 
-void early_end()
-{
+suite early_end = [] {
    using namespace boost::ut;
 
    "early_end"_test = [] {
@@ -942,8 +937,7 @@ void early_end()
       std::string buffer_data =
          R"({"thing":{"a":3.14/*Test comment 1*/,"b":"stuff"/*Test comment 2*/},"thing2array":[{"a":3.14/*Test comment 1*/,"b":"stuff"/*Test comment 2*/,"c":999.342494903,"d":1e-12,"e":203082348402.1,"f":89.089,"g":12380.00000013,"h":1000000.000001}],"vec3":[3.14,2.7,6.5],"list":[6,7,8,2],"deque":[9,6.7,3.1],"vector":[[9,6.7,3.1],[3.14,2.7,6.5]],"i":8,"d":2/*double is the best type*/,"b":false,"c":"W","vb":[true,false,false,true,true,true,true],"sptr":{"a":3.14/*Test comment 1*/,"b":"stuff"/*Test comment 2*/},"optional":null,"array":["as\"df\\ghjkl","pie","42","foo"],"map":{"a":4,"b":12,"f":7},"mapi":{"2":9.63,"5":3.14,"7":7.42},"thing_ptr":{"a":3.14/*Test comment 1*/,"b":"stuff"/*Test comment 2*/}})";
       std::string_view buffer = buffer_data;
-      while (buffer.size() > 0)
-      {
+      while (buffer.size() > 0) {
          buffer_data.pop_back();
          buffer = buffer_data;
          // This is mainly to check if all our end checks are in place.
@@ -958,10 +952,9 @@ void early_end()
          expect(err.location <= buffer.size());
       }
    };
-}
+};
 
-void bench()
-{
+suite bench = [] {
    using namespace boost::ut;
    "bench"_test = [] {
       std::cout << "\nPerformance regresion test: \n";
@@ -981,13 +974,10 @@ void bench()
          glz::write_json(thing, buffer);
       }
       auto tend = std::chrono::high_resolution_clock::now();
-      auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(
-                         tend - tstart)
-                         .count();
+      auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(tend - tstart).count();
       auto mbytes_per_sec = repeat * buffer.size() / (duration * 1048576);
       std::cout << "write_json size: " << buffer.size() << " bytes\n";
-      std::cout << "write_json: " << duration << " s, " << mbytes_per_sec
-                << " MB/s"
+      std::cout << "write_json: " << duration << " s, " << mbytes_per_sec << " MB/s"
                 << "\n";
 
       tstart = std::chrono::high_resolution_clock::now();
@@ -995,26 +985,21 @@ void bench()
          expect(glz::read_json(thing, buffer) == glz::error_code::none);
       }
       tend = std::chrono::high_resolution_clock::now();
-      duration = std::chrono::duration_cast<std::chrono::duration<double>>(
-                    tend - tstart)
-                    .count();
+      duration = std::chrono::duration_cast<std::chrono::duration<double>>(tend - tstart).count();
       mbytes_per_sec = repeat * buffer.size() / (duration * 1048576);
-      std::cout << "read_json: " << duration << " s, " << mbytes_per_sec
-                     << " MB/s" << "\n";
+      std::cout << "read_json: " << duration << " s, " << mbytes_per_sec << " MB/s"
+                << "\n";
 
       tstart = std::chrono::high_resolution_clock::now();
       for (size_t i{}; i < repeat; ++i) {
          glz::get<std::string>(thing, "/thing_ptr/b");
       }
       tend = std::chrono::high_resolution_clock::now();
-      duration = std::chrono::duration_cast<std::chrono::duration<double>>(
-                    tend - tstart)
-                    .count();
-      std::cout << "get: " << duration << " s, " << (repeat / duration)
-                << " gets/s"
+      duration = std::chrono::duration_cast<std::chrono::duration<double>>(tend - tstart).count();
+      std::cout << "get: " << duration << " s, " << (repeat / duration) << " gets/s"
                 << "\n\n";
    };
-}
+};
 
 struct v3
 {
@@ -1045,7 +1030,7 @@ struct glz::meta<oob>
   static constexpr auto value = object("v", &oob::v, "n", &oob::n);
 };
 
-void read_tests() {
+suite read_tests = [] {
    using namespace boost::ut;
    
    "string read"_test = [] {
@@ -1546,7 +1531,7 @@ void read_tests() {
       expect(m[2][0] == 5.0);
       expect(m[2][3] == 4.0);
    };
-}
+};
 
 using Geodetic = v3;
 
@@ -1615,7 +1600,7 @@ struct glz::meta<EmptyObject>
    static constexpr auto value = object();
 };
 
-void write_tests() {
+suite write_tests = [] {
    using namespace boost::ut;
 
    "Write floating point types"_test = [] {
@@ -1911,7 +1896,7 @@ void write_tests() {
       glz::write_json(m, buf);
       expect(buf == R"({"1":[4,0,0],"2":[5,0,0,4]})");
    };
-}
+};
 
 suite error_outputs = [] {
    "invalid character"_test = [] {
@@ -1940,55 +1925,52 @@ struct glz::meta<study_obj>
    static constexpr auto value = object("x", &T::x, "y", &T::y);
 };
 
-void study_tests()
-{
+suite study_tests = [] {
    "study"_test = [] {
       glz::study::design design;
-      design.params = { {.ptr = "/x", .distribution = "linspace", .range = { "0", "1", "10" } } };
-      
-      glz::study::full_factorial generator{ study_obj{}, design };
-      
+      design.params = {{.ptr = "/x", .distribution = "linspace", .range = {"0", "1", "10"}}};
+
+      glz::study::full_factorial generator{study_obj{}, design};
+
       std::vector<size_t> results;
       std::mutex mtx{};
-      glz::study::run_study(generator, [&](const auto& point, [[maybe_unused]] const auto job_num){
+      glz::study::run_study(generator, [&](const auto& point, [[maybe_unused]] const auto job_num) {
          std::unique_lock lock{mtx};
          results.emplace_back(point.value().x);
       });
-      
+
       std::sort(results.begin(), results.end());
-      
+
       expect(results[0] == 0);
       expect(results[10] == 10);
    };
-   
+
    "doe"_test = [] {
       glz::study::design design;
-      design.params = { { "/x", "linspace", { "0", "1", "3" } },
-         { "/y", "linspace", { "0", "1", "2" } }
-      };
-      
-      glz::study::full_factorial g{ study_obj{}, design };
-      
+      design.params = {{"/x", "linspace", {"0", "1", "3"}}, {"/y", "linspace", {"0", "1", "2"}}};
+
+      glz::study::full_factorial g{study_obj{}, design};
+
       std::vector<std::string> results;
       for (size_t i = 0; i < g.size(); ++i) {
          const auto point = g.generate(i).value();
          results.emplace_back(std::to_string(point.x) + "|" + std::to_string(point.y));
       }
-      
+
       std::sort(results.begin(), results.end());
-      
+
       std::vector<std::string> results2;
       std::mutex mtx{};
-      glz::study::run_study(g, [&](const auto& point, [[maybe_unused]] const auto job_num){
+      glz::study::run_study(g, [&](const auto& point, [[maybe_unused]] const auto job_num) {
          std::unique_lock lock{mtx};
          results2.emplace_back(std::to_string(point.value().x) + "|" + std::to_string(point.value().y));
       });
-      
+
       std::sort(results2.begin(), results2.end());
-      
+
       expect(results == results2);
    };
-}
+};
 
 suite thread_pool = [] {
    "thread pool"_test = [] {
@@ -2482,47 +2464,45 @@ struct glz::meta<includer_struct>
    static constexpr auto value = object("#include", glz::file_include{}, "str", &T::str, "i", &T::i);
 };
 
-void file_include_test()
-{
+suite file_include_test = [] {
    includer_struct obj{};
-   
+
    expect(glz::write_file_json(obj, "../alabastar.json") == glz::error_code::none);
-   
+
    obj.str = "";
-   
+
    std::string s = R"({"#include": "../alabastar.json", "i": 100})";
    expect(glz::read_json(obj, s) == glz::error_code::none);
-   
+
    expect(obj.str == "Hello") << obj.str;
    expect(obj.i == 100) << obj.i;
-   
+
    obj.str = "";
-   
+
    glz::read_file_json(obj, "../alabastar.json");
    expect(obj.str == "Hello") << obj.str;
    expect(obj.i == 55) << obj.i;
-}
+};
 
-void file_include_test_auto()
-{
+suite file_include_test_auto = [] {
    includer_struct obj{};
-   
+
    expect(glz::write_file(obj, "./auto.json") == false);
-   
+
    obj.str = "";
-   
+
    std::string s = R"({"#include": "./auto.json", "i": 100})";
    expect(glz::read_json(obj, s) == glz::error_code::none);
-   
+
    expect(obj.str == "Hello") << obj.str;
    expect(obj.i == 100) << obj.i;
-   
+
    obj.str = "";
-   
+
    glz::read_file(obj, "./auto.json");
    expect(obj.str == "Hello") << obj.str;
    expect(obj.i == 55) << obj.i;
-}
+};
 
 struct nested0
 {
@@ -2538,34 +2518,33 @@ struct glz::meta<nested0>
    static constexpr auto value = object("#include", glz::file_include{}, "a", &T::a, "b", &T::b);
 };
 
-void nested_file_include_test()
-{
+suite nested_file_include_test = [] {
    nested0 obj;
-   
+
    std::string a = R"({"#include": "../b/b.json"})";
    {
       std::filesystem::create_directory("a");
-      std::ofstream a_file{ "./a/a.json" };
-      
+      std::ofstream a_file{"./a/a.json"};
+
       a_file << a;
    }
-   
+
    {
       std::filesystem::create_directory("b");
-      
+
       obj.b.i = 13;
-      
+
       expect(glz::write_file_json(obj.b, "./b/b.json") == glz::error_code::none);
    }
-   
+
    obj.b.i = 0;
-   
+
    std::string s = R"({ "a": { "#include": "./a/a.json" }, "b": { "#include": "./b/b.json" } })";
-   
+
    expect(glz::read_json(obj, s) == glz::error_code::none);
-   
+
    expect(obj.a.i == 13);
-}
+};
 
 suite shrink_to_fit = [] {
    "shrink_to_fit"_test = [] {
@@ -3939,20 +3918,5 @@ suite metaobject_variant_auto_deduction = [] {
 
 int main()
 {
-   using namespace boost::ut;
-   
-   basic_types();
-   container_types();
-   nullable_types();
-   enum_types();
-   user_types();
-   json_pointer();
-   early_end(); 
-   bench();
-   read_tests();
-   write_tests();
-   study_tests();
-   file_include_test();
-   file_include_test_auto();
-   nested_file_include_test();
+   //Suites are set to run on exit
 }
