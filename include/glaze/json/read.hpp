f180dfe88ed44d73491c62e62a8e74a85b4f5a05
Stephen Berry <stephenberry.developer@gmail.com>
2023-02-27 15:41:15 -0600

    fixes for glz::visit

--- include/glaze/json/read.hpp
+++ include/glaze/json/read.hpp
@@ -728,7 +728,7 @@ namespace glz
                
                auto itr = map.find(s);
                if (itr != map.end()) {
-                  std::visit([&](auto&& x) {
+                  glz::visit([&](auto&& x) {
                      get_member(value, x) = true;
                   }, itr->second);
                }
@@ -988,7 +988,7 @@ namespace glz
                   static constexpr auto frozen_map = detail::make_map<T, Opts.allow_hash_check>();
                   const auto& member_it = frozen_map.find(key);
                   if (member_it != frozen_map.end()) [[likely]] {
-                     std::visit(
+                     glz::visit(
                         [&](auto&& member_ptr) {
                            read<json>::op<Opts>(get_member(value, member_ptr), ctx, it, end);
                         },
@@ -1130,7 +1130,7 @@ namespace glz
                                         it = start;
                                         const auto type_index = id_it->second;
                                         if (value.index() != type_index) value = runtime_variant_map<T>()[type_index];
-                                        std::visit(
+                                        glz::visit(
                                            [&](auto&& v) {
                                               using V = std::decay_t<decltype(v)>;
                                               if constexpr (glaze_object_t<V>) {
@@ -1165,7 +1165,7 @@ namespace glz
                                it = start;
                                const auto type_index = possible_types.countr_zero();
                                if (value.index() != type_index) value = runtime_variant_map<T>()[type_index];
-                               std::visit(
+                               glz::visit(
                                   [&](auto&& v) {
                                      using V = std::decay_t<decltype(v)>;
                                      if constexpr (glaze_object_t<V>) {
@@ -1252,7 +1252,7 @@ namespace glz
                 }
              }
              else {
-                std::visit(
+                glz::visit(
                    [&](auto&& v) {
                       using V = std::decay_t<decltype(v)>;
                       read<json>::op<Opts>(v, ctx, it, end);
--- include/glaze/util/variant.hpp
+++ include/glaze/util/variant.hpp
@@ -13,13 +13,25 @@
 
 #define GLZ_REPEAT_32_I(x)                                                                                           \
    {                                                                                                                 \
-      x(1) x(2) x(3) x(4) x(5) x(6) x(7) x(8) x(9) x(10) x(11) x(12) x(13) x(14) x(15) x(16) x(17) x(18) x(19) x(20) \
-         x(21) x(22) x(23) x(24) x(25) x(26) x(27) x(28) x(29) x(30) x(31) x(32)                                     \
+      x(0) x(1) x(2) x(3) x(4) x(5) x(6) x(7) x(8) x(9) x(10) x(11) x(12) x(13) x(14) x(15) x(16) x(17) x(18) x(19) x(20) \
+         x(21) x(22) x(23) x(24) x(25) x(26) x(27) x(28) x(29) x(30) x(31)                                     \
    }
-
 
 namespace glz
 {
+   [[noreturn]] inline void unreachable() noexcept {
+      // Uses compiler specific extensions if possible.
+      // Even if no extension is used, undefined behavior is still raised by
+      // an empty function body and the noreturn attribute.
+   #ifdef __GNUC__
+      // GCC, Clang, ICC
+      __builtin_unreachable();
+   #elifdef _MSC_VER
+      // MSVC
+      __assume(false);
+   #endif
+   }
+   
    template <class T>
    concept is_variant = is_specialization_v<T, std::variant>;
 
@@ -60,25 +72,21 @@ namespace glz
 #define GLZ_VISIT_CASE(N)                                                                       \
    case I + N: {                                                                             \
       if constexpr (I + N < vs) {                                                            \
-         return std::forward<F>(f)(*std::get_if<std::variant_alternative_t<I + N, dV>>(&std::forward<V>(v))); \
+         return std::forward<F>(f)(*std::get_if<I + N>(&std::forward<V>(v))); \
       }                                                                                      \
    }                                                                                         \
-      /**/
 
          switch (v.index()) {
             GLZ_REPEAT_32_I(GLZ_VISIT_CASE)
          }
 #undef GLZ_VISIT_CASE
 
-         constexpr auto next_idx = (std::min)(I + 32, vs);
+         constexpr auto next_idx = I + 32;
 
-         // if constexpr(next_idx < vs) causes some weird msvc bug
-         if constexpr (next_idx + 0 < vs) {
+         if constexpr (next_idx < vs) {
             return visit1<next_idx>(std::forward<F>(f), std::forward<V>(v));
          }
-         else {
-            return std::forward<F>(f)(std::get<0>(std::forward<V>(v)));
-         }
+         unreachable();
       }
    }
 }
