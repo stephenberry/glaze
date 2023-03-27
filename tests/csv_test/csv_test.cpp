6cde8eb6787a94de978999ad597f657bf555f2e3
Stephen Berry <stephenberry.developer@gmail.com>
2023-03-27 19:41:48 +0000

    clang format

--- tests/csv_test/csv_test.cpp
+++ tests/csv_test/csv_test.cpp
@@ -113,7 +113,7 @@ y,1,2,3,4,5,6,7,8,9,10
       // m.clear();
       // read<opts{.format = csv}>(m, out);
    };
-   
+
    "std::unordered_map"_test = [] {
       std::unordered_map<std::string, std::vector<uint64_t>> m;
       auto& x = m["x"];
