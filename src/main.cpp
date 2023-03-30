b568f894972e4b836b8cd542c9508f1ccbeff4a6
mwalcott3 <mwalcott3@users.noreply.github.com>
2023-03-30 09:49:11 -0500

    Test auto formatting action

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,19 @@
 #include "glaze/glaze.hpp"
 
 struct obj_t
-{
-   double x{};
+{double x{};
    float y{};
 };
 
 template <>
 struct glz::meta<obj_t>
-{
-   using T = obj_t;
+{using T = obj_t;
    static constexpr auto value = object("x", &T::x);
 };
 
 int main()
-{
+                  {
    std::string buffer{};
-   obj_t obj{};
-   glz::write_json(obj, buffer);
-   std::ignore = glz::read_json(obj, buffer);
-   return 0;
-}
+obj_t obj{};
+   glz::write_json(obj, buffer);std::ignore = glz::read_json(obj, buffer);
+   return 0;}
