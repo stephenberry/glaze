82e58160f834ecc9d34981edc3c50d48d8892a04
stephenberry <stephenberry@users.noreply.github.com>
2023-06-06 15:29:27 +0000

    Committing clang-format changes

--- include/glaze/json/json_ptr.hpp
+++ include/glaze/json/json_ptr.hpp
@@ -567,7 +567,7 @@ namespace glz
       auto it = p.first;
       auto end = p.second;
 
-      //using span_t = std::span<std::remove_pointer_t<std::remove_reference_t<decltype(it)>>>;
+      // using span_t = std::span<std::remove_pointer_t<std::remove_reference_t<decltype(it)>>>;
       using span_t = std::span<const char>; // TODO: should be more generic, but currently broken with mingw
 
       auto start = it;
