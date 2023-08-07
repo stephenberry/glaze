52e96b4402c5d2bcfcf70b60fab54c075bf86c85
Stephen Berry <stephenberry.developer@gmail.com>
2023-08-07 13:18:16 -0500

    cleaning

--- include/glaze/binary/read.hpp
+++ include/glaze/binary/read.hpp
@@ -322,25 +322,16 @@ namespace glz
          template <auto Opts>
          GLZ_ALWAYS_INLINE static void op(T& value, is_context auto&& ctx, auto&& it, auto&& end) noexcept
          {
+            using Key = typename T::first_type;
+            
+            constexpr uint8_t type = str_t<Key> ? 0 : (std::unsigned_integral<Key> ? 0b000'10'000 : 0b000'01'000);
+            constexpr uint8_t byte_count = str_t<Key> ? 1 : sizeof(Key);
+            constexpr uint8_t header = tag::object | type | (byte_count << 5);
+            
             const auto tag = uint8_t(*it);
-            if ((tag & 0b00000'111) != tag::object) {
+            if (tag != header) {
                ctx.error = error_code::syntax_error;
                return;
-            }
-
-            using Key = typename T::first_type;
-            if constexpr (str_t<Key>) {
-               if (get_bits<3, 2>(tag) != 0) {
-                  ctx.error = error_code::syntax_error;
-                  return;
-               }
-            }
-            else {
-               static constexpr uint8_t type_id = 1 + std::unsigned_integral<Key>;
-               if (get_bits<3, 2>(tag) != type_id) {
-                  ctx.error = error_code::syntax_error;
-                  return;
-               }
             }
 
             ++it;
