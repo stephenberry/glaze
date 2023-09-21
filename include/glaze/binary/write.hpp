a209b1ee3c0c7f6a6a179f8bb6ae24fed91510bd
Stephen Berry <stephenberry.developer@gmail.com>
2023-09-21 07:52:07 -0500

    better compile time binary errors

--- include/glaze/binary/write.hpp
+++ include/glaze/binary/write.hpp
@@ -76,6 +76,12 @@ namespace glz
       template <class T = void>
       struct to_binary
       {};
+      
+      template <auto Opts, class T, class Ctx, class B, class IX>
+      concept write_binary_invocable = requires(T&& value, Ctx&& ctx, B&& b, IX&& ix) {
+         to_binary<std::remove_cvref_t<T>>::template op<Opts>(std::forward<T>(value), std::forward<Ctx>(ctx),
+                                                              std::forward<B>(b), std::forward<IX>(ix));
+      };
 
       template <>
       struct write<binary>
@@ -83,8 +89,13 @@ namespace glz
          template <auto Opts, class T, is_context Ctx, class B, class IX>
          GLZ_ALWAYS_INLINE static void op(T&& value, Ctx&& ctx, B&& b, IX&& ix) noexcept
          {
-            to_binary<std::remove_cvref_t<T>>::template op<Opts>(std::forward<T>(value), std::forward<Ctx>(ctx),
-                                                                 std::forward<B>(b), std::forward<IX>(ix));
+            if constexpr (write_binary_invocable<Opts, T, Ctx, B, IX>) {
+               to_binary<std::remove_cvref_t<T>>::template op<Opts>(std::forward<T>(value), std::forward<Ctx>(ctx),
+                                                                    std::forward<B>(b), std::forward<IX>(ix));
+            }
+            else {
+               static_assert(false_v<T>, "Glaze metadata is probably needed for your type");
+            }
          }
 
          template <auto Opts, class T, is_context Ctx, class B, class IX>
