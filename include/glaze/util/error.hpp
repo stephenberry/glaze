f7b4e71db5f089dd847e5d8f1c6f517e85dfa249
Stephen Berry <stephenberry.developer@gmail.com>
2023-09-25 10:09:03 -0400

    remove unused code

--- include/glaze/util/error.hpp
+++
@@ -1,9 +0,0 @@
-// Glaze Library
-// For the license information refer to glaze.hpp
-
-#pragma once
-
-namespace glz
-{
-   enum class error : uint32_t { none, read, write, maximum_size_exceeded, mismatching_dimensions };
-}
--- include/glaze/util/variant.hpp
+++ include/glaze/util/variant.hpp
@@ -12,12 +12,6 @@
 #include "glaze/util/parse.hpp"
 #include "glaze/util/type_traits.hpp"
 
-#define GLZ_REPEAT_32_I(x)                                                                                          \
-   {                                                                                                                \
-      x(0) x(1) x(2) x(3) x(4) x(5) x(6) x(7) x(8) x(9) x(10) x(11) x(12) x(13) x(14) x(15) x(16) x(17) x(18) x(19) \
-         x(20) x(21) x(22) x(23) x(24) x(25) x(26) x(27) x(28) x(29) x(30) x(31)                                    \
-   }
-
 namespace glz
 {
    [[noreturn]] inline void unreachable() noexcept
