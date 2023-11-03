2ebbd0e62331ecc023cd01c43980143798172c4b
Stephen Berry <stephenberry.developer@gmail.com>
2023-11-03 08:37:05 -0500

    added glz::escaped wrapper

--- include/glaze/json/raw_string.hpp
+++ include/glaze/json/raw_string.hpp
@@ -18,6 +18,12 @@ namespace glz
    {
       T& val;
    };
+   
+   template <class T>
+   struct escaped_t
+   {
+      T& val;
+   };
 
    namespace detail
    {
@@ -40,14 +46,43 @@ namespace glz
             write<json>::op<opt_true<Opts, &opts::raw_string>>(value.val, ctx, args...);
          }
       };
+      
+      template <class T>
+      struct from_json<escaped_t<T>>
+      {
+         template <auto Opts>
+         GLZ_ALWAYS_INLINE static void op(auto&& value, auto&&... args) noexcept
+         {
+            read<json>::op<opt_false<Opts, &opts::raw_string>>(value.val, args...);
+         }
+      };
+
+      template <class T>
+      struct to_json<escaped_t<T>>
+      {
+         template <auto Opts>
+         GLZ_ALWAYS_INLINE static void op(auto&& value, is_context auto&& ctx, auto&&... args) noexcept
+         {
+            write<json>::op<opt_false<Opts, &opts::raw_string>>(value.val, ctx, args...);
+         }
+      };
 
       template <auto MemPtr>
       GLZ_ALWAYS_INLINE constexpr decltype(auto) raw_string_impl() noexcept
       {
          return [](auto&& val) { return raw_string_t<std::remove_reference_t<decltype(val.*MemPtr)>>{val.*MemPtr}; };
       }
+      
+      template <auto MemPtr>
+      GLZ_ALWAYS_INLINE constexpr decltype(auto) escaped_impl() noexcept
+      {
+         return [](auto&& val) { return escaped_t<std::remove_reference_t<decltype(val.*MemPtr)>>{val.*MemPtr}; };
+      }
    }
 
    template <auto MemPtr>
    constexpr auto raw_string = detail::raw_string_impl<MemPtr>();
+   
+   template <auto MemPtr>
+   constexpr auto escaped = detail::escaped_impl<MemPtr>();
 }
