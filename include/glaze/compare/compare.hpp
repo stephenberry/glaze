d916b88d337d780fc94d9f43386bd7f22f1f8222
Stephen Berry <stephenberry.developer@gmail.com>
2023-11-22 11:32:10 -0600

    using standard comparisons

--- include/glaze/compare/compare.hpp
+++ include/glaze/compare/compare.hpp
@@ -5,6 +5,8 @@
 
 #include "glaze/core/common.hpp"
 
+#include <functional>
+
 namespace glz
 {
    struct equal_to final
@@ -21,7 +23,7 @@ namespace glz
             for_each<N>([&](auto I) {
                auto& l = detail::get_member(lhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
                auto& r = detail::get_member(rhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
-               if (l != r) {
+               if (!std::equal_to{}(l, r)) {
                   equal = false;
                }
             });
@@ -41,7 +43,26 @@ namespace glz
          for_each<N>([&](auto I) {
             auto& l = detail::get_member(lhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
             auto& r = detail::get_member(rhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
-            if (l >= r) {
+            if (!std::less{}(l, r)) {
+               less_than = false;
+            }
+         });
+         
+         return less_than;
+      }
+   };
+   
+   struct less_equal final
+   {
+      template <detail::glaze_object_t T>
+      constexpr bool operator()(T&& lhs, T&& rhs) noexcept {
+         constexpr auto N = std::tuple_size_v<meta_t<T>>;
+         
+         bool less_than = true;
+         for_each<N>([&](auto I) {
+            auto& l = detail::get_member(lhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
+            auto& r = detail::get_member(rhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
+            if (!std::less_equal{}(l, r)) {
                less_than = false;
             }
          });
@@ -60,7 +81,26 @@ namespace glz
          for_each<N>([&](auto I) {
             auto& l = detail::get_member(lhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
             auto& r = detail::get_member(rhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
-            if (l <= r) {
+            if (!std::greater{}(l, r)) {
+               greater_than = false;
+            }
+         });
+         
+         return greater_than;
+      }
+   };
+   
+   struct greater_equal final
+   {
+      template <detail::glaze_object_t T>
+      constexpr bool operator()(T&& lhs, T&& rhs) noexcept {
+         constexpr auto N = std::tuple_size_v<meta_t<T>>;
+         
+         bool greater_than = true;
+         for_each<N>([&](auto I) {
+            auto& l = detail::get_member(lhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
+            auto& r = detail::get_member(rhs, tuplet::get<1>(tuplet::get<I>(meta_v<T>)));
+            if (!std::greater_equal{}(l, r)) {
                greater_than = false;
             }
          });
