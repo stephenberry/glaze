d74f151bf43a159013f04e90512accf2eb3be693
Stephen Berry <stephenberry.developer@gmail.com>
2023-11-22 15:35:01 -0600

    fix for get namespace change

--- include/glaze/reflection/reflect.hpp
+++ include/glaze/reflection/reflect.hpp
@@ -31,7 +31,7 @@ namespace glz
       constexpr auto to_names(auto& out, auto, auto... args)
       {
          if constexpr (sizeof...(args) > 1) {
-            out.push_back(tuplet::get<2>(tuplet::tuple{args...}));
+            out.push_back(get<2>(tuplet::tuple{args...}));
          }
       }
 
