d03cc6d1e64563f2e8f68504eef41f28887df0f2
Stephen Berry <stephenberry.developer@gmail.com>
2023-11-27 14:12:15 -0600

    MSVC reflection

--- include/glaze/reflection/get_name.hpp
+++ include/glaze/reflection/get_name.hpp
@@ -15,6 +15,31 @@
 
 namespace glz
 {
+   template <class T>
+   extern const T fake_object;
+
+   template <class T>
+   struct remove_member_pointer {
+       using type = T;
+   };
+
+   template <class C, class T>
+   struct remove_member_pointer<T C::*> {
+       using type = C;
+   };
+
+   template <auto P>
+   constexpr std::string_view get_name_msvc() noexcept {
+       std::string_view str = GLZ_PRETTY_FUNCTION;
+
+       size_t i = str.find("->");
+       str = str.substr(i + 2);
+       i = str.find(">");
+       str = str.substr(0, i);
+
+       return str;
+   }
+   
 #if defined(__clang__)
    inline constexpr auto pretty_function_tail = "]";
 #elif defined(__GNUC__) || defined(__GNUG__)
@@ -27,8 +52,9 @@ namespace glz
    constexpr std::string_view get_name() noexcept
    {
 #if defined(_MSC_VER)
-      static_assert(false_v<decltype(P)>, "MSVC does not support member variable name reflection");
-      return {};
+    using T = remove_member_pointer<std::decay_t<decltype(P)>>::type;
+    constexpr auto p = P;
+    return get_name_msvc<&(fake_object<T>.*p)>();
 #else
       // TODO: Use std::source_location when deprecating clang 14
       // std::string_view str = std::source_location::current().function_name();
