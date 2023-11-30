6f8f83b63be2fb88de3a0060526c1600324a88fb
Stephen Berry <stephenberry.developer@gmail.com>
2023-11-30 09:16:39 -0600

    Using SWAR in stoui64

--- include/glaze/util/stoui64.hpp
+++ include/glaze/util/stoui64.hpp
@@ -21,6 +21,10 @@ namespace glz::detail
       constexpr auto b = (std::numeric_limits<uint64_t>::max)() / 10;
       return a <= b;
    }
+   
+   GLZ_ALWAYS_INLINE constexpr uint64_t is_greater_7(const uint64_t c) noexcept {
+      return (c & 0b1111100011111000111110001111100011111000111110001111100011111000);
+   }
 
    template <class T = uint64_t>
    GLZ_ALWAYS_INLINE constexpr bool stoui64(uint64_t& res, const char*& c, [[maybe_unused]] const char* end) noexcept
@@ -29,21 +33,61 @@ namespace glz::detail
          return false;
       }
 
-      // maximum number of digits need is: 3, 5, 10, 20, for byte sizes of 1, 2, 4, 8
-      // we need to store one extra space for a digit for sizes of 1, 2, and 4 because we avoid checking for overflow
+      // Maximum number of digits need is: 3, 5, 10, 20, for byte sizes of 1, 2, 4, 8
+      // We need to store one extra space for a digit for sizes of 1, 2, and 4 because we avoid checking for overflow
       // since we store in a uint64_t
-      constexpr std::array<uint32_t, 4> max_digits_from_size = {4, 6, 11, 20};
+      // However, use 8 byte alignment so we can use SWAR, and we want to have extra space for next_digit increments for '0' and '.'
+      // we need to have 8 bytes available after the period
+      constexpr std::array<uint32_t, 4> digits_end = {4, 6, 11, 20}; // for non SWAR approach
+      constexpr auto digits_length = digits_end[std::bit_width(sizeof(T)) - 1];
+      constexpr std::array<uint32_t, 4> max_digits_from_size = {16, 16, 24, 32};
       constexpr auto N = max_digits_from_size[std::bit_width(sizeof(T)) - 1];
 
       std::array<uint8_t, N> digits{0};
       auto next_digit = digits.data();
-      auto end_digit = digits.data() + digits.size();
-      auto consume_digit = [&] {
-         if (next_digit < end_digit) [[likely]] {
-            *next_digit = (*c - '0');
-            ++next_digit;
+      auto end_digit = digits.data() + digits_length;
+      const auto buffer_available = std::distance(c, end);
+      auto consume_digits = [&] {
+         if (buffer_available > 31) {
+            while (next_digit < end_digit) {
+               uint64_t chunk;
+               std::memcpy(&chunk, c, 8);
+               chunk -= 0x3030303030303030ULL; // subtract 48 from bytes
+               std::memcpy(next_digit, &chunk, 8);
+               
+               const uint64_t test_chars = is_greater_7(chunk);
+               if (test_chars) {
+                  const auto length = (std::countr_zero(test_chars) >> 3);
+                  c += length;
+                  next_digit += length;
+                  if (*c == '8') {
+                     ++c;
+                     ++next_digit;
+                  }
+                  else if (*c == '9') {
+                     ++c;
+                     ++next_digit;
+                  }
+                  else {
+                     std::memset(next_digit, 0, size_t(end_digit - next_digit));
+                     break;
+                  }
+               }
+               else {
+                  c += 8;
+                  next_digit += 8;
+               }
+            }
          }
-         ++c;
+         else {
+            while (is_digit(*c)) {
+               if (next_digit < end_digit) [[likely]] {
+                  *next_digit = (*c - '0');
+                  ++next_digit;
+               }
+               ++c;
+            }
+         }
       };
 
       if (*c == '0') {
@@ -56,16 +100,12 @@ namespace glz::detail
          }
       }
 
-      while (is_digit(*c)) {
-         consume_digit();
-      }
+      consume_digits();
       auto n = std::distance(digits.data(), next_digit);
 
       if (*c == '.') {
          ++c;
-         while (is_digit(*c)) {
-            consume_digit();
-         }
+         consume_digits();
       }
 
       if (*c == 'e' || *c == 'E') {
@@ -106,7 +146,7 @@ namespace glz::detail
                return false;
             }
             if (is_safe_addition(res, digits[19])) [[likely]] {
-               res += digits.back();
+               res += digits[19];
             }
             else [[unlikely]] {
                return false;
