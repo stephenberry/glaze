3aefc2b5bfab070fe36abe3cdd99473964fe4981
Stephen Berry <stephenberry.developer@gmail.com>
2023-12-05 14:36:21 -0600

    Cleaning up reflection code

---
+++ include/glaze/reflection/reflect_binary.hpp
@@ -0,0 +1,14 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#pragma once
+
+#include "glaze/binary/read.hpp"
+#include "glaze/binary/write.hpp"
+#include "glaze/reflection/get_name.hpp"
+#include "glaze/reflection/to_tuple.hpp"
+
+namespace glz::detail
+{
+   
+}
