cbfc524c78cce58b222eee6a79ed62be01b78d82
Stephen Berry <stephenberry.developer@gmail.com>
2023-12-06 20:36:35 -0600

    Cleaning code (#594)
    * removed unnecessary ifdef
    
    * Update common.hpp

--- include/glaze/core/common.hpp
+++ include/glaze/core/common.hpp
@@ -896,7 +896,7 @@ namespace glz
          else if constexpr (std::is_member_function_pointer_v<V>) {
             return member_ptr;
          }
-         else if constexpr (std::invocable<MemPtr, decltype(value)>) {
+         else if constexpr (std::invocable<MemPtr, Value>) {
             return std::invoke(std::forward<MemPtr>(member_ptr), std::forward<Value>(value));
          }
          else if constexpr (std::is_pointer_v<V>) {
