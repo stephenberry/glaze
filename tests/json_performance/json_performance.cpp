3bd8c07f8fe06218dc7751635a48ef580eedf999
Stephen Berry <stephenberry.developer@gmail.com>
2023-12-19 16:17:21 -0600

    performance test for JSON string reading

---
+++ tests/json_performance/json_performance.cpp
@@ -0,0 +1,61 @@
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <random>
+#include <iostream>
+
+#include "boost/ut.hpp"
+#include "glaze/glaze.hpp"
+
+using namespace boost::ut;
+
+std::mt19937 gen{};
+
+static constexpr std::string_view charset{ "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~\"\\\r\b\f\t\n" };
+
+inline std::string generate_string() {
+   auto length = std::uniform_int_distribution<uint32_t>{0, 512}(gen);
+   static int32_t charsetSize = charset.size();
+   std::uniform_int_distribution<uint32_t> distribution(0, charsetSize - 1);
+   std::string result{};
+   result.reserve(length);
+   for (uint32_t x = 0; x < length; ++x) {
+      result += charset[distribution(gen)];
+   }
+   return result;
+}
+
+suite string_performance = [] {
+   
+   "string_performance"_test = [] {
+      constexpr auto n = 1000000;
+      
+      std::vector<std::string> vec;
+      vec.reserve(n);
+      
+      std::mt19937_64 g{};
+      
+      for (size_t i = 0; i < n; ++i) {
+         vec.emplace_back(generate_string());
+      }
+      
+      std::string buffer;
+      glz::write_json(vec, buffer);
+      vec.clear();
+      auto t0 = std::chrono::steady_clock::now();
+      const auto e = glz::read_json(vec, buffer);
+      auto t1 = std::chrono::steady_clock::now();
+      
+      expect(!e);
+      
+      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() * 1e-6;
+      std::cerr << duration << '\n';
+   };
+};
+
+int main()
+{ // Explicitly run registered test suites and report errors
+   // This prevents potential issues with thread local variables
+   const auto result = boost::ut::cfg<>.run({.report_errors = true});
+   return result;
+}
