c0bcb0af3c80b1133e01aa7aceabec10483a5baa
Stephen Berry <stephenberry.developer@gmail.com>
2023-12-21 11:45:21 -0600

    Update json_performance.cpp

--- tests/json_performance/json_performance.cpp
+++ tests/json_performance/json_performance.cpp
@@ -29,7 +29,7 @@ inline std::string generate_string()
 
 suite string_performance = [] {
    "string_performance"_test = [] {
-      constexpr auto n = 10000; // make this number bigger when profiling
+      constexpr auto n = 100; // make this number bigger when profiling
 
       std::vector<std::string> vec;
       vec.reserve(n);
