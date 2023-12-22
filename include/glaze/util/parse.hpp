72e0f93a299bf31353c56760716e3cb1c0f4bc4b
Stephen Berry <stephenberry.developer@gmail.com>
2023-12-22 06:18:38 -0600

    better key code and errors now point to start of key

--- include/glaze/util/parse.hpp
+++ include/glaze/util/parse.hpp
@@ -429,7 +429,7 @@ namespace glz::detail
    }
 
    // very similar code to skip_till_quote, but it consumes the iterator and returns the key
-   template <uint32_t MinLength, uint32_t LengthRange>
+   template <uint32_t MinLength, uint32_t LengthRange> requires (LengthRange < 16)
    [[nodiscard]] GLZ_ALWAYS_INLINE const sv parse_key_cx(auto&& it) noexcept
    {
       static_assert(std::contiguous_iterator<std::decay_t<decltype(it)>>);
@@ -437,12 +437,11 @@ namespace glz::detail
       auto start = it;
       it += MinLength; // immediately skip minimum length
 
-      static_assert(LengthRange < 16);
       if constexpr (LengthRange == 7) {
          uint64_t chunk; // no need to default initialize
          std::memcpy(&chunk, it, 8);
          const uint64_t test_chunk = has_quote(chunk);
-         if (test_chunk != 0) [[likely]] {
+         if (test_chunk) [[likely]] {
             it += (std::countr_zero(test_chunk) >> 3);
 
             return {start, size_t(it - start)};
@@ -452,7 +451,7 @@ namespace glz::detail
          uint64_t chunk; // no need to default initialize
          std::memcpy(&chunk, it, 8);
          uint64_t test_chunk = has_quote(chunk);
-         if (test_chunk != 0) {
+         if (test_chunk) {
             it += (std::countr_zero(test_chunk) >> 3);
 
             return {start, size_t(it - start)};
@@ -463,7 +462,7 @@ namespace glz::detail
             chunk = 0; // must zero out the chunk
             std::memcpy(&chunk, it, rest);
             test_chunk = has_quote(chunk);
-            if (test_chunk != 0) {
+            if (test_chunk) {
                it += (std::countr_zero(test_chunk) >> 3);
 
                return {start, size_t(it - start)};
@@ -474,14 +473,14 @@ namespace glz::detail
          uint64_t chunk{};
          std::memcpy(&chunk, it, LengthRange + 1);
          const uint64_t test_chunk = has_quote(chunk);
-         if (test_chunk != 0) [[likely]] {
+         if (test_chunk) [[likely]] {
             it += (std::countr_zero(test_chunk) >> 3);
 
             return {start, size_t(it - start)};
          }
       }
 
-      return {start, size_t(it - start)};
+      return {};
    }
 
    template <opts Opts>
