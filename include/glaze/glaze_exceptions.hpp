40df322beea95b23656b7bb4094cb2d660a4cd7e
Stephen Berry <stephenberry.developer@gmail.com>
2024-01-03 13:13:41 -0600

    deprecated bad read_file and write_file (#679)

--- include/glaze/glaze_exceptions.hpp
+++ include/glaze/glaze_exceptions.hpp
@@ -43,6 +43,7 @@ namespace glz::ex
 namespace glz::ex
 {
    template <class T>
+   [[deprecated("Use specific read_file_json, etc. This old version was a bad design and would instatiate all formats")]]
    void read_file(T& value, const sv file_name, auto&& buffer)
    {
       const auto ec = glz::read_file(value, file_name, buffer);
@@ -55,6 +56,7 @@ namespace glz::ex
    }
 
    template <class T>
+   [[deprecated("Use specific read_file_json, etc. This old version was a bad design and would instatiate all formats")]]
    void write_file(T& value, const sv file_name, auto&& buffer)
    {
       const auto ec = glz::write_file(value, file_name, buffer);
