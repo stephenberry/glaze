de26961445f58947f12c1896dd11a1273f36dd2b
Stephen Berry <stephenberry.developer@gmail.com>
2024-01-09 07:25:24 -0600

    Update dev-mode.cmake

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -1,6 +1,9 @@
 enable_language(CXX)
 
-cmake_policy(SET CMP0135 NEW)
+# Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP in CMake 3.24:
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+  cmake_policy(SET CMP0135 NEW)
+endif()
 
 set_property(GLOBAL PROPERTY USE_FOLDERS YES)
 
