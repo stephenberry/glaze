e2b92df8a04e1b81963dcb12353a0d8c10ab8ccc
Stephen Berry <stephenberry.developer@gmail.com>
2024-01-09 08:05:59 -0600

    Cmake cleanup (#696)
    * Hiding dev warning
    
    * Update dev-mode.cmake
    
    * Update boost ut

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -1,5 +1,10 @@
 enable_language(CXX)
 
+# Avoid warning about DOWNLOAD_EXTRACT_TIMESTAMP in CMake 3.24:
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+  cmake_policy(SET CMP0135 NEW)
+endif()
+
 set_property(GLOBAL PROPERTY USE_FOLDERS YES)
 
 include(CTest)
