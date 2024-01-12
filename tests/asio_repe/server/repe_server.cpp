8aa616aebad9b21855c1f6530af1ed1c5208b5b0
Stephen Berry <stephenberry.developer@gmail.com>
2024-01-12 09:52:30 -0600

    adding asio repe test projects

---
+++ tests/asio_repe/server/repe_server.cpp
@@ -0,0 +1,19 @@
+// Glaze Library
+// For the license information refer to glaze.hpp
+
+#ifndef BOOST_UT_DISABLE_MODULE
+#define BOOST_UT_DISABLE_MODULE
+#endif
+
+#include "boost/ut.hpp"
+
+using namespace boost::ut;
+
+#include "glaze/glaze.hpp"
+#include "glaze/rpc/repe.hpp"
+
+int main()
+{
+   const auto result = boost::ut::cfg<>.run({.report_errors = true});
+   return result;
+}
