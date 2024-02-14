a16f6220a947e0f5994fef514b7cf4617c41947f
Stephen Berry <stephenberry.developer@gmail.com>
2024-02-14 12:25:25 -0600

    Fixed test case

--- tests/reflection_test/reflection_test.cpp
+++ tests/reflection_test/reflection_test.cpp
@@ -444,7 +444,7 @@ struct glz::meta<meta_schema_t>
 {
    schema x{.description = "x is a special integer"};
    schema file_name{.description = "provide a file name to load"};
-   schema boolean{.description = "for validation"};
+   schema is_valid{.description = "for validation"};
 };
 
 static_assert(glz::detail::reflectable<glz::meta<meta_schema_t>>);
@@ -458,7 +458,7 @@ suite meta_schema_reflection_tests = [] {
       expect(buffer == R"({"x":0,"file_name":"","is_valid":false})") << buffer;
       
       const auto json_schema = glz::write_json_schema<meta_schema_t>();
-      expect(json_schema == R"({"type":["object"],"properties":{"file_name":{"$ref":"#/$defs/std::string","description":"provide a file name to load"},"is_valid":{"$ref":"#/$defs/bool"},"x":{"$ref":"#/$defs/int32_t","description":"x is a special integer"}},"additionalProperties":false,"$defs":{"bool":{"type":["boolean"]},"int32_t":{"type":["integer"]},"std::string":{"type":["string"]}}})") << json_schema;
+      expect(json_schema == R"({"type":["object"],"properties":{"file_name":{"$ref":"#/$defs/std::string","description":"provide a file name to load"},"is_valid":{"$ref":"#/$defs/bool","description":"for validation"},"x":{"$ref":"#/$defs/int32_t","description":"x is a special integer"}},"additionalProperties":false,"$defs":{"bool":{"type":["boolean"]},"int32_t":{"type":["integer"]},"std::string":{"type":["string"]}}})") << json_schema;
    };
 };
 
