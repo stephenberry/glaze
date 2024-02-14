daffda9c676c2720fa7348f2aa2910c8316691d4
stephenberry <stephenberry@users.noreply.github.com>
2024-02-14 22:30:55 +0000

    Committing clang-format changes

--- include/glaze/core/common.hpp
+++ include/glaze/core/common.hpp
@@ -294,14 +294,13 @@ namespace glz
       static constexpr std::string_view name = "raw_json";
    };
 
-   using basic =
-      std::variant<bool, char, char8_t, unsigned char, signed char, short, unsigned short,
-                   float, int, unsigned int, long, unsigned long, double, long long, unsigned long long, std::string>;
+   using basic = std::variant<bool, char, char8_t, unsigned char, signed char, short, unsigned short, float, int,
+                              unsigned int, long, unsigned long, double, long long, unsigned long long, std::string>;
 
    // Explicitly defining basic_ptr to avoid additional template instantiations
-   using basic_ptr = std::variant<bool*, char*, char8_t*, unsigned char*, signed char*, short*,
-                                  unsigned short*, float*, int*, unsigned int*, long*,
-                                  unsigned long*, double*, long long*, unsigned long long*, std::string*>;
+   using basic_ptr =
+      std::variant<bool*, char*, char8_t*, unsigned char*, signed char*, short*, unsigned short*, float*, int*,
+                   unsigned int*, long*, unsigned long*, double*, long long*, unsigned long long*, std::string*>;
 
    namespace detail
    {
