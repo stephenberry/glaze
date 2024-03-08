cef87e06445b29dd717a91d228125553f1f6dd5a
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-08 11:20:23 -0600

    tuplet naming cleanup

--- include/glaze/api/tuplet.hpp
+++ include/glaze/api/tuplet.hpp
@@ -10,21 +10,20 @@ namespace glz
 {
    namespace detail
    {
-      template <class Tuple, size_t... I>
+      template <class... T, size_t... I>
       constexpr std::string_view tuplet_name_impl(std::index_sequence<I...>)
       {
          return join_v<chars<"glz::tuplet::tuple<">,
-                       ((I != std::tuple_size_v<Tuple> - 1) ? join_v<name_v<std::tuple_element_t<I, Tuple>>, chars<",">>
-                                                            : join_v<name_v<std::tuple_element_t<I, Tuple>>>)...,
+                       ((I != sizeof(T) - 1) ? join_v<name_v<T>, chars<",">>
+                                                            : join_v<name_v<T>>)...,
                        chars<">">>;
       }
    }
 
-   template <class T>
-      requires is_specialization_v<T, glz::tuplet::tuple>
-   struct meta<T>
+   template <class... T>
+   struct meta<glz::tuplet::tuple<T...>>
    {
       static constexpr std::string_view name =
-         detail::tuplet_name_impl<T>(std::make_index_sequence<std::tuple_size_v<T>>());
+         detail::tuplet_name_impl<T...>(std::make_index_sequence<sizeof...(T)>());
    };
 }
