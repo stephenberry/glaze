94680edf97e77a1f525bbcc28398722cb3027e47
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-13 13:23:08 -0500

    Improving asio client initialization

--- tests/asio_repe/client/repe_client.cpp
+++ tests/asio_repe/client/repe_client.cpp
@@ -10,24 +10,45 @@
 void asio_client_test()
 {
    try {
-      glz::asio_client<> client{"localhost", "8080"};
-      client.init();
-
-      std::vector<int> data{};
-      for (int i = 1; i < 10; ++i) {
-         data.emplace_back(i);
-         int sum{};
-         if (auto ec = client.call({"/sum"}, data, sum); ec) {
-            std::cerr << glz::write_json(ec) << '\n';
-         }
-         else {
-            std::cout << sum << '\n';
-         }
+      constexpr auto N = 100;
+      std::vector<glz::asio_client<>> clients;
+      clients.reserve(N);
+      
+      std::vector<std::future<void>> threads;
+      threads.reserve(N);
+      
+      for (size_t i = 0; i < N; ++i) {
+         clients.emplace_back(glz::asio_client<>{"localhost", "8080"});
       }
-
-      auto sum = client.callable<int(const std::vector<int>&)>({"/sum"});
-
-      std::cout << "callable result: " << sum(std::vector<int>{1, 2, 3}) << '\n';
+      
+      for (size_t i = 0; i < N; ++i) {
+         threads.emplace_back(std::async([&, i]{
+            auto& client = clients[i];
+            const auto ec = client.init();
+            if (!ec) {
+                std::cout << "Connected to server" << std::endl;
+            } else {
+                std::cerr << "Error: " << ec.message() << std::endl;
+            }
+            
+            std::vector<int> data;
+            for (int j = 1; j < 100; ++j) {
+               data.emplace_back(j);
+            }
+            
+            int sum{};
+            if (auto ec = client.call({"/sum"}, data, sum); ec) {
+               std::cerr << glz::write_json(ec) << '\n';
+            }
+            else {
+               std::cout << "i: " << i << ", " <<  sum << '\n';
+            }
+         }));
+      }
+      
+      for (auto& t : threads) {
+         t.get();
+      }
    }
    catch (const std::exception& e) {
       std::cerr << e.what() << '\n';
