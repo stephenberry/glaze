3b5d7eec73dd8b97f2d67eae707a6f9e3a726b2a
stephenberry <stephenberry@users.noreply.github.com>
2024-03-13 19:25:52 +0000

    Committing clang-format changes

--- tests/asio_repe/client/repe_client.cpp
+++ tests/asio_repe/client/repe_client.cpp
@@ -13,39 +13,40 @@ void asio_client_test()
       constexpr auto N = 100;
       std::vector<glz::asio_client<>> clients;
       clients.reserve(N);
-      
+
       std::vector<std::future<void>> threads;
       threads.reserve(N);
-      
+
       for (size_t i = 0; i < N; ++i) {
          clients.emplace_back(glz::asio_client<>{"localhost", "8080"});
       }
-      
+
       for (size_t i = 0; i < N; ++i) {
-         threads.emplace_back(std::async([&, i]{
+         threads.emplace_back(std::async([&, i] {
             auto& client = clients[i];
             const auto ec = client.init();
             if (!ec) {
-                std::cout << "Connected to server" << std::endl;
-            } else {
-                std::cerr << "Error: " << ec.message() << std::endl;
+               std::cout << "Connected to server" << std::endl;
             }
-            
+            else {
+               std::cerr << "Error: " << ec.message() << std::endl;
+            }
+
             std::vector<int> data;
             for (int j = 1; j < 100; ++j) {
                data.emplace_back(j);
             }
-            
+
             int sum{};
             if (auto ec = client.call({"/sum"}, data, sum); ec) {
                std::cerr << glz::write_json(ec) << '\n';
             }
             else {
-               std::cout << "i: " << i << ", " <<  sum << '\n';
+               std::cout << "i: " << i << ", " << sum << '\n';
             }
          }));
       }
-      
+
       for (auto& t : threads) {
          t.get();
       }
