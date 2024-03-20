ef52f2a70845a1617fdf05bc47e31365c138de1b
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-20 09:38:34 -0500

    Using std::forward in for_each

--- include/glaze/util/for_each.hpp
+++ include/glaze/util/for_each.hpp
@@ -10,8 +10,8 @@ namespace glz
    template <class = void, std::size_t... Is>
    constexpr auto indexer(std::index_sequence<Is...>) noexcept
    {
-      return [](auto&& f) noexcept -> decltype(auto) {
-         return decltype(f)(f)(std::integral_constant<std::size_t, Is>{}...);
+      return []<class F>(F&& f) noexcept -> decltype(auto) {
+         return std::forward<F>(f)(std::integral_constant<std::size_t, Is>{}...);
       };
    }
 
@@ -37,7 +37,7 @@ namespace glz
    template <class = void, std::size_t... Is>
    constexpr auto indexer_ex(std::index_sequence<Is...>)
    {
-      return [](auto&& f) -> decltype(auto) { return decltype(f)(f)(std::integral_constant<std::size_t, Is>{}...); };
+      return []<class F>(F&& f) -> decltype(auto) { return std::forward<F>(f)(std::integral_constant<std::size_t, Is>{}...); };
    }
 
    // takes a number N
