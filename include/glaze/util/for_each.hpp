467e5f8088d6796737ebaa3a6ff28974861b2451
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-25 06:43:21 -0500

    Much cleaner for_each

--- include/glaze/util/for_each.hpp
+++ include/glaze/util/for_each.hpp
@@ -7,53 +7,11 @@
 
 namespace glz
 {
-   template <class = void, std::size_t... Is>
-   constexpr auto indexer(std::index_sequence<Is...>) noexcept
-   {
-      return []<class F>(F&& f) noexcept -> decltype(auto) {
-         return std::forward<F>(f)(std::integral_constant<std::size_t, Is>{}...);
-      };
-   }
-
-   // takes a number N
-   // returns a function object that, when passed a function object f
-   // passes it compile-time values from 0 to N-1 inclusive.
-   template <size_t N>
-   constexpr auto indexer() noexcept
-   {
-      return indexer(std::make_index_sequence<N>{});
-   }
-
-   template <size_t N, class Func>
-   constexpr auto for_each(Func&& f) noexcept
-   {
-      return indexer<N>()([&](auto&&... i) noexcept { (std::forward<Func>(f)(i), ...); });
-   }
-}
-
-// versions that allow exceptions to propagate, for when users implement functions that throw
-namespace glz
-{
-   template <class = void, std::size_t... Is>
-   constexpr auto indexer_ex(std::index_sequence<Is...>)
-   {
-      return []<class F>(F&& f) -> decltype(auto) {
-         return std::forward<F>(f)(std::integral_constant<std::size_t, Is>{}...);
-      };
-   }
-
-   // takes a number N
-   // returns a function object that, when passed a function object f
-   // passes it compile-time values from 0 to N-1 inclusive.
-   template <size_t N>
-   constexpr auto indexer_ex()
-   {
-      return indexer_ex(std::make_index_sequence<N>{});
-   }
-
-   template <size_t N, class Func>
-   constexpr auto for_each_ex(Func&& f)
-   {
-      return indexer_ex<N>()([&](auto&&... i) { (std::forward<Func>(f)(i), ...); });
+   // We do not make this noexcept so that it can be used in exception contexts
+   template <std::size_t N, class Func>
+   constexpr void for_each(Func&& f) {
+      [&]<std::size_t... I>(std::index_sequence<I...>) {
+         (f(std::integral_constant<std::size_t, I>{}), ...);
+      }(std::make_index_sequence<N>{});
    }
 }
