fec4ecc526a1804dd7c97cbc3608f263cd4a41bb
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-25 08:53:27 -0500

    Update tuplet.hpp

--- include/glaze/api/tuplet.hpp
+++ include/glaze/api/tuplet.hpp
@@ -8,19 +8,12 @@
 
 namespace glz
 {
-   namespace detail
-   {
-      template <class... T, size_t... I>
-      constexpr std::string_view tuplet_name_impl(std::index_sequence<I...>)
-      {
-         return join_v<chars<"glz::tuplet::tuple<">,
-                       ((I != sizeof...(T) - 1) ? join_v<name_v<T>, chars<",">> : join_v<name_v<T>>)..., chars<">">>;
-      }
-   }
-
    template <class... T>
    struct meta<glz::tuplet::tuple<T...>>
    {
-      static constexpr std::string_view name = detail::tuplet_name_impl<T...>(std::make_index_sequence<sizeof...(T)>());
+      static constexpr std::string_view name = []<size_t... I>(std::index_sequence<I...>) {
+         return join_v<chars<"glz::tuplet::tuple<">,
+                       ((I != sizeof...(T) - 1) ? join_v<name_v<T>, chars<",">> : join_v<name_v<T>>)..., chars<">">>;
+      }(std::make_index_sequence<sizeof...(T)>{});
    };
 }
