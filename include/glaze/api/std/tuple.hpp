49b38026a2798ae5fce80013cb03a99de79d57fa
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-25 08:27:48 -0500

    Update tuple.hpp

--- include/glaze/api/std/tuple.hpp
+++ include/glaze/api/std/tuple.hpp
@@ -9,19 +9,12 @@
 
 namespace glz
 {
-   namespace detail
-   {
-      template <class... T, size_t... I>
-      constexpr std::string_view tuple_name_impl(std::index_sequence<I...>)
-      {
-         return join_v<chars<"std::tuple<">,
-                       ((I != sizeof...(T) - 1) ? join_v<name_v<T>, chars<",">> : join_v<name_v<T>>)..., chars<">">>;
-      }
-   }
-
    template <class... T>
    struct meta<std::tuple<T...>>
    {
-      static constexpr std::string_view name = detail::tuple_name_impl<T...>(std::make_index_sequence<sizeof...(T)>());
+      static constexpr std::string_view name = []<size_t... I>(std::index_sequence<I...>) {
+         return join_v<chars<"std::tuple<">,
+                       ((I != sizeof...(T) - 1) ? join_v<name_v<T>, chars<",">> : join_v<name_v<T>>)..., chars<">">>;
+      }(std::make_index_sequence<sizeof...(T)>{});
    };
 }
