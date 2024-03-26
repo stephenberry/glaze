49ca549f6df32008b031e4f0a10a6e12d3505b9c
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-26 04:22:15 -0500

    Handling tabs in error formatting context

--- include/glaze/util/validate.hpp
+++ include/glaze/util/validate.hpp
@@ -20,6 +20,16 @@ namespace glz
          size_t front_truncation{};
          size_t rear_truncation{};
       };
+      
+      // We convert to only single spaces for error messages in order to keep the source info
+      // calculation more efficient and avoid needing to allocate more memory.
+      inline void convert_tabs_to_single_spaces(std::string& input) noexcept {
+          for (auto& c : input) {
+              if (c == '\t') {
+                  c = ' ';
+              }
+          }
+      }
 
       inline std::optional<source_info> get_source_info(const std::string_view buffer, const size_t index)
       {
@@ -63,10 +73,12 @@ namespace glz
          if constexpr (std::same_as<V, std::byte>) {
             std::string context{reinterpret_cast<const char*>(&(*context_begin)),
                                 reinterpret_cast<const char*>(&(*context_end))};
+            convert_tabs_to_single_spaces(context);
             return source_info{line, column, context, index, front_truncation, rear_truncation};
          }
          else {
             std::string context{context_begin, context_end};
+            convert_tabs_to_single_spaces(context);
             return source_info{line, column, context, index, front_truncation, rear_truncation};
          }
       }
