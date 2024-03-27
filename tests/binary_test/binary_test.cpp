92813b0c7eaf7c0a2b9584f6dd12833d365f3933
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-27 16:01:57 -0500

    object test with std::filesystem::path

--- tests/binary_test/binary_test.cpp
+++ tests/binary_test/binary_test.cpp
@@ -1606,14 +1606,28 @@ suite merge_tests = [] {
    };
 };
 
+struct path_test_struct {
+    uint32_t i{0};
+    std::filesystem::path p{"./my_path"};
+};
+
 suite filesystem_tests = [] {
-   "std::filesystem_path"_test = [] {
+   "std::filesystem::path"_test = [] {
       std::filesystem::path p{"./my_path"};
       std::string buffer = glz::write_binary(p);
       
       p = "./bogus";
       expect(!glz::read_binary(p, buffer));
       expect(p.string() == "./my_path");
+   };
+   
+   "path_test_struct"_test = [] {
+      path_test_struct obj{};
+      std::string buffer = glz::write_binary(obj);
+      
+      obj.p.clear();
+      expect(!glz::read_binary(obj, buffer));
+      expect(obj.p == "./my_path");
    };
 };
 
