52cce885bf88ff6a192a33ca669e81fe6adf8d0b
Stephen Berry <stephenberry.developer@gmail.com>
2024-03-29 06:42:19 -0500

    Using asio::transfer_exactly

--- include/glaze/ext/glaze_asio.hpp
+++ include/glaze/ext/glaze_asio.hpp
@@ -30,16 +30,16 @@ namespace glz
    inline asio::awaitable<void> co_send_buffer(asio::ip::tcp::socket& socket, const std::string_view str)
    {
       const uint64_t size = str.size();
-      co_await asio::async_write(socket, asio::buffer(&size, sizeof(uint64_t)), asio::use_awaitable);
-      co_await asio::async_write(socket, asio::buffer(str), asio::use_awaitable);
+      co_await asio::async_write(socket, asio::buffer(&size, sizeof(uint64_t)), asio::transfer_exactly(sizeof(uint64_t)), asio::use_awaitable);
+      co_await asio::async_write(socket, asio::buffer(str), asio::transfer_exactly(size), asio::use_awaitable);
    }
 
    inline asio::awaitable<void> co_receive_buffer(asio::ip::tcp::socket& socket, std::string& str)
    {
       uint64_t size;
-      co_await asio::async_read(socket, asio::buffer(&size, sizeof(size)), asio::use_awaitable);
+      co_await asio::async_read(socket, asio::buffer(&size, sizeof(size)), asio::transfer_exactly(sizeof(uint64_t)), asio::use_awaitable);
       str.resize(size);
-      co_await asio::async_read(socket, asio::buffer(str), asio::use_awaitable);
+      co_await asio::async_read(socket, asio::buffer(str), asio::transfer_exactly(size), asio::use_awaitable);
    }
 
    inline asio::awaitable<void> call_rpc(asio::ip::tcp::socket& socket, std::string& buffer)
