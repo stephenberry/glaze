48133bea90043d2ea4954859d73b5c857f9e8465
Stephen Berry <stephenberry.developer@gmail.com>
2024-04-24 04:07:36 -0500

    reusing read_iterators

--- include/glaze/core/read.hpp
+++ include/glaze/core/read.hpp
@@ -50,40 +50,25 @@ namespace glz
    [[nodiscard]] parse_error read(T& value, detail::contiguous auto&& buffer, is_context auto&& ctx) noexcept
    {
       static_assert(sizeof(decltype(*buffer.data())) == 1);
-
-      auto b = reinterpret_cast<const char*>(buffer.data());
-      auto e = reinterpret_cast<const char*>(buffer.data()); // to be incremented
+      
+      auto [b, e] = read_iterators<Opts>(ctx, buffer);
+      if (bool(ctx.error)) [[unlikely]] {
+         return {ctx.error, 0};
+      }
 
       auto start = b;
 
-      using Buffer = std::decay_t<decltype(buffer)>;
-      if constexpr (is_specialization_v<Buffer, std::basic_string> ||
-                    is_specialization_v<Buffer, std::basic_string_view> || span<Buffer>) {
-         e += buffer.size();
-
-         if (b == e) {
-            ctx.error = error_code::no_read_input;
-            return {ctx.error, 0};
-         }
-      }
-      else {
-         // if not a std::string, std::string_view, or span, check that the last character is a null character
-         if (buffer.empty()) {
-            ctx.error = error_code::no_read_input;
-            return {ctx.error, 0};
-         }
-         e += buffer.size() - 1;
-         if (*e != '\0') {
-            ctx.error = error_code::data_must_be_null_terminated;
-            return {ctx.error, 0};
-         }
-      }
-
       detail::read<Opts.format>::template op<Opts>(value, ctx, b, e);
+      if (bool(ctx.error)) [[unlikely]] {
+         return {ctx.error, size_t(b - start), ctx.includer_error};
+      }
 
       if constexpr (Opts.force_conformance) {
          if (b < e) {
-            detail::skip_ws<Opts>(ctx, b, e);
+            detail::skip_ws_no_pre_check<Opts>(ctx, b, e);
+            if (bool(ctx.error)) [[unlikely]] {
+               return {ctx.error, size_t(b - start), ctx.includer_error};
+            }
             if (b != e) {
                ctx.error = error_code::syntax_error;
             }
