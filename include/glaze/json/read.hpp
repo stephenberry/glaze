49bde83410d43aa1b58a56ee7b2e31fda54da76a
Stephen Berry <stephenberry.developer@gmail.com>
2024-04-23 22:22:59 -0500

    remove old dereference

--- include/glaze/json/read.hpp
+++ include/glaze/json/read.hpp
@@ -515,10 +515,10 @@ namespace glz
 
                      const char* c;
                      if (length < size_t(end - it)) [[likely]] {
-                        c = parse_string<Bytes>(&*start, value.data(), ctx);
+                        c = parse_string<Bytes>(start, value.data(), ctx);
                      }
                      else [[unlikely]] {
-                        c = parse_string<1>(&*start, value.data(), ctx);
+                        c = parse_string<1>(start, value.data(), ctx);
                      }
 
                      if (bool(ctx.error)) [[unlikely]] {
