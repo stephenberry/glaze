1e0e760c7a205d44a6d1914c769f09f7dbfd3df4
Stephen Berry <stephenberry.developer@gmail.com>
2024-06-17 09:49:47 -0500

    formatting

--- include/glaze/socket/socket.hpp
+++ include/glaze/socket/socket.hpp
@@ -4,34 +4,35 @@
 #pragma once
 
 #ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    #pragma comment(lib, "Ws2_32.lib")
-    #define CLOSESOCKET closesocket
-    #define SOCKET_ERROR_CODE WSAGetLastError()
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+#define CLOSESOCKET closesocket
+#define SOCKET_ERROR_CODE WSAGetLastError()
 #else
-    #include <sys/types.h>
-    #include <sys/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #if __has_include(<netinet/in.h>)
-    #include <netinet/in.h>
+#include <netinet/in.h>
 #endif
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <netinet/tcp.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
-    #include <fcntl.h>
-   #include <cerrno>
-    #define CLOSESOCKET close
-    #define SOCKET_ERROR_CODE errno
-    #define SOCKET int
+#include <unistd.h>
+
+#include <cerrno>
+#define CLOSESOCKET close
+#define SOCKET_ERROR_CODE errno
+#define SOCKET int
 #endif
 
-#include <iostream>
-#include <thread>
-#include <vector>
+#include <format>
 #include <functional>
+#include <iostream>
 #include <mutex>
 #include <system_error>
-#include <format>
+#include <thread>
+#include <vector>
 
 #include "glaze/thread/threadpool.hpp"
 
@@ -40,227 +41,232 @@ namespace glz
    namespace ip
    {
       struct opts
-      {
-         
-      };
+      {};
    }
-   
+
    inline void wsa_startup()
    {
       static std::once_flag flag{};
-      std::call_once(flag, []{
+      std::call_once(flag, [] {
 #ifdef _WIN32
          WSADATA wsaData;
          WSAStartup(MAKEWORD(2, 2), &wsaData);
 #endif
       });
    }
-   
-   enum ip_error
+
+   enum ip_error { none = 0, socket_connect_failed = 1001, server_bind_failed = 1002 };
+
+   struct ip_error_category : public std::error_category
    {
-      none = 0,
-      socket_connect_failed = 1001,
-      server_bind_failed = 1002
-   };
-   
-   struct ip_error_category : public std::error_category {
-       static const ip_error_category& instance() {
-          static ip_error_category instance{};
-           return instance;
-       }
+      static const ip_error_category& instance()
+      {
+         static ip_error_category instance{};
+         return instance;
+      }
 
-       const char* name() const noexcept override {
-           return "ip_error_category";
-       }
+      const char* name() const noexcept override { return "ip_error_category"; }
 
-       std::string message(int ec) const override {
-          using enum ip_error;
-           switch (static_cast<ip_error>(ec)) {
-               case socket_connect_failed:
-                   return "socket_connect_failed";
-              case server_bind_failed:
-                  return "server_bind_failed";
-               default:
-                   return "unknown_error";
-           }
-       }
+      std::string message(int ec) const override
+      {
+         using enum ip_error;
+         switch (static_cast<ip_error>(ec)) {
+         case socket_connect_failed:
+            return "socket_connect_failed";
+         case server_bind_failed:
+            return "server_bind_failed";
+         default:
+            return "unknown_error";
+         }
+      }
    };
-   
-   struct socket {
-       using Callback = std::function<void(const std::string&, ssize_t)>;
-      
+
+   struct socket
+   {
+      using Callback = std::function<void(const std::string&, ssize_t)>;
+
       SOCKET socket_fd{-1};
 
-       void set_non_blocking(SOCKET fd) {
-           #ifdef _WIN32
-               u_long mode = 1;
-               ioctlsocket(fd, FIONBIO, &mode);
-           #else
-               int flags = fcntl(fd, F_GETFL, 0);
-               fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-           #endif
-       }
-      
-      socket() {
-         wsa_startup();
+      void set_non_blocking(SOCKET fd)
+      {
+#ifdef _WIN32
+         u_long mode = 1;
+         ioctlsocket(fd, FIONBIO, &mode);
+#else
+         int flags = fcntl(fd, F_GETFL, 0);
+         fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+#endif
       }
-      
-      socket(SOCKET fd) : socket_fd(fd) {
+
+      socket() { wsa_startup(); }
+
+      socket(SOCKET fd) : socket_fd(fd)
+      {
          wsa_startup();
          set_non_blocking(socket_fd);
       }
 
-       ~socket() {
-           if (socket_fd != -1) {
-               CLOSESOCKET(socket_fd);
-           }
-           #ifdef _WIN32
-               WSACleanup();
-           #endif
-       }
+      ~socket()
+      {
+         if (socket_fd != -1) {
+            CLOSESOCKET(socket_fd);
+         }
+#ifdef _WIN32
+         WSACleanup();
+#endif
+      }
 
-       std::error_code connect(const std::string& address, int port) {
-           socket_fd = ::socket(AF_INET, SOCK_STREAM, 0);
-           if (socket_fd == -1) {
-              return {ip_error::socket_connect_failed, ip_error_category::instance()};
-           }
+      std::error_code connect(const std::string& address, int port)
+      {
+         socket_fd = ::socket(AF_INET, SOCK_STREAM, 0);
+         if (socket_fd == -1) {
+            return {ip_error::socket_connect_failed, ip_error_category::instance()};
+         }
 
-           sockaddr_in server_addr;
-           server_addr.sin_family = AF_INET;
-           server_addr.sin_port = htons(port);
-           inet_pton(AF_INET, address.c_str(), &server_addr.sin_addr);
+         sockaddr_in server_addr;
+         server_addr.sin_family = AF_INET;
+         server_addr.sin_port = htons(port);
+         inet_pton(AF_INET, address.c_str(), &server_addr.sin_addr);
 
-           if (::connect(socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-              return {ip_error::socket_connect_failed, ip_error_category::instance()};
-           }
+         if (::connect(socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+            return {ip_error::socket_connect_failed, ip_error_category::instance()};
+         }
 
-           set_non_blocking(socket_fd);
+         set_non_blocking(socket_fd);
 
-          return {};
-       }
-      
-      bool no_delay() {
-           int flag = 1;
-           int result = setsockopt(socket_fd, IPPROTO_TCP, TCP_NODELAY, (char*)&flag, sizeof(int));
-           return result == 0;
-       }
-      
-      bool bind_and_listen(int port) {
-           socket_fd = ::socket(AF_INET, SOCK_STREAM, 0);
-           if (socket_fd == -1) {
-               return false;
-           }
+         return {};
+      }
 
-           sockaddr_in server_addr;
-           server_addr.sin_family = AF_INET;
-           server_addr.sin_addr.s_addr = INADDR_ANY;
-           server_addr.sin_port = htons(port);
+      bool no_delay()
+      {
+         int flag = 1;
+         int result = setsockopt(socket_fd, IPPROTO_TCP, TCP_NODELAY, (char*)&flag, sizeof(int));
+         return result == 0;
+      }
 
-           if (::bind(socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-               return false;
-           }
+      bool bind_and_listen(int port)
+      {
+         socket_fd = ::socket(AF_INET, SOCK_STREAM, 0);
+         if (socket_fd == -1) {
+            return false;
+         }
 
-           if (::listen(socket_fd, SOMAXCONN) == -1) {
-               return false;
-           }
+         sockaddr_in server_addr;
+         server_addr.sin_family = AF_INET;
+         server_addr.sin_addr.s_addr = INADDR_ANY;
+         server_addr.sin_port = htons(port);
 
-           set_non_blocking(socket_fd);
-          no_delay();
+         if (::bind(socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+            return false;
+         }
 
-           return true;
-       }
+         if (::listen(socket_fd, SOMAXCONN) == -1) {
+            return false;
+         }
 
-       void read(Callback callback) {
-          std::string buffer('\0', 1024);
-          while (true) {
-              ssize_t bytes_read = ::recv(int(socket_fd), buffer.data(), buffer.size(), 0);
-              if (bytes_read > 0) {
-                  callback(buffer, bytes_read);
-              } else if (bytes_read == 0) {
-                  break; // connection has been closed
-              } else {
-                  if (SOCKET_ERROR_CODE != EWOULDBLOCK && SOCKET_ERROR_CODE != EAGAIN) {
-                      break;
-                  }
-              }
-              std::this_thread::sleep_for(std::chrono::milliseconds(100));
-          }
-       }
+         set_non_blocking(socket_fd);
+         no_delay();
 
-       void write(const std::string& data, Callback callback) {
-          size_t total_bytes_sent = 0;
-          while (total_bytes_sent < data.size()) {
-              ssize_t bytes_sent = ::send(int(socket_fd), data.data() + total_bytes_sent, data.size() - total_bytes_sent, 0);
-              if (bytes_sent > 0) {
-                  total_bytes_sent += bytes_sent;
-              } else {
-                  if (SOCKET_ERROR_CODE != EWOULDBLOCK && SOCKET_ERROR_CODE != EAGAIN) {
-                      break;
-                  }
-              }
-              std::this_thread::sleep_for(std::chrono::milliseconds(100));
-          }
-          callback(data, total_bytes_sent);
-       }
+         return true;
+      }
+
+      void read(Callback callback)
+      {
+         std::string buffer('\0', 1024);
+         while (true) {
+            ssize_t bytes_read = ::recv(int(socket_fd), buffer.data(), buffer.size(), 0);
+            if (bytes_read > 0) {
+               callback(buffer, bytes_read);
+            }
+            else if (bytes_read == 0) {
+               break; // connection has been closed
+            }
+            else {
+               if (SOCKET_ERROR_CODE != EWOULDBLOCK && SOCKET_ERROR_CODE != EAGAIN) {
+                  break;
+               }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+         }
+      }
+
+      void write(const std::string& data, Callback callback)
+      {
+         size_t total_bytes_sent = 0;
+         while (total_bytes_sent < data.size()) {
+            ssize_t bytes_sent =
+               ::send(int(socket_fd), data.data() + total_bytes_sent, data.size() - total_bytes_sent, 0);
+            if (bytes_sent > 0) {
+               total_bytes_sent += bytes_sent;
+            }
+            else {
+               if (SOCKET_ERROR_CODE != EWOULDBLOCK && SOCKET_ERROR_CODE != EAGAIN) {
+                  break;
+               }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+         }
+         callback(data, total_bytes_sent);
+      }
    };
-   
-   struct destructor final {
-       std::function<void()> destroy{};
 
-       ~destructor() {
-           if (destroy) {
-               destroy();
-           }
-       }
+   struct destructor final
+   {
+      std::function<void()> destroy{};
 
-       template <class Func>
-       destructor(Func&& f) : destroy(std::forward<Func>(f)) {}
+      ~destructor()
+      {
+         if (destroy) {
+            destroy();
+         }
+      }
+
+      template <class Func>
+      destructor(Func&& f) : destroy(std::forward<Func>(f))
+      {}
    };
-   
+
    struct server final
    {
       int port{};
       glz::pool threads{1};
-      
+
       std::atomic<bool> active = true;
-      
-      destructor on_destruct{[this]{
-         active = false;
-      }};
-      
+
+      destructor on_destruct{[this] { active = false; }};
+
       using AcceptCallback = std::function<void(socket&&)>;
-      
+
       template <class AcceptCallback>
-      std::error_code async_accept(AcceptCallback&& callback) {
+      std::error_code async_accept(AcceptCallback&& callback)
+      {
          std::unique_ptr<glz::socket> accept_socket = std::make_unique<glz::socket>();
-         
-         if (accept_socket->bind_and_listen(port))
-         {
-            std::thread([this, accept_socket = std::move(accept_socket), callback = std::forward<AcceptCallback>(callback)]() {
+
+         if (accept_socket->bind_and_listen(port)) {
+            std::thread([this, accept_socket = std::move(accept_socket),
+                         callback = std::forward<AcceptCallback>(callback)]() {
                while (active) {
-                   sockaddr_in client_addr;
-                   socklen_t client_len = sizeof(client_addr);
+                  sockaddr_in client_addr;
+                  socklen_t client_len = sizeof(client_addr);
                   // As long as we're not calling accept on the same port we are safe
-                   SOCKET client_fd = ::accept(accept_socket->socket_fd, (sockaddr*)&client_addr, &client_len);
-                   if (client_fd != -1) {
-                      threads.emplace_back([callback = std::move(callback), client_fd]{
-                         callback(socket(client_fd));
-                      });
-                   } else {
-                       if (SOCKET_ERROR_CODE != EWOULDBLOCK && SOCKET_ERROR_CODE != EAGAIN) {
-                           break;
-                       }
-                   }
-                   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                  SOCKET client_fd = ::accept(accept_socket->socket_fd, (sockaddr*)&client_addr, &client_len);
+                  if (client_fd != -1) {
+                     threads.emplace_back([callback = std::move(callback), client_fd] { callback(socket(client_fd)); });
+                  }
+                  else {
+                     if (SOCKET_ERROR_CODE != EWOULDBLOCK && SOCKET_ERROR_CODE != EAGAIN) {
+                        break;
+                     }
+                  }
+                  std::this_thread::sleep_for(std::chrono::milliseconds(10));
                }
             }).detach();
          }
          else {
             return {ip_error::server_bind_failed, ip_error_category::instance()};
          }
-         
+
          return {};
-       }
+      }
    };
 }
-
--- tests/socket_test/socket_test.cpp
+++ tests/socket_test/socket_test.cpp
@@ -3,48 +3,47 @@
 
 #define UT_RUN_TIME_ONLY
 
+#include "glaze/socket/socket.hpp"
+
+#include <future>
 #include <latch>
 #include <thread>
 
 #include "ut/ut.hpp"
-
-#include "glaze/socket/socket.hpp"
-
-#include <future>
 
 using namespace ut;
 
 std::future<void> server_thread{};
 
 suite make_server = [] {
-   server_thread = std::async([]{
+   server_thread = std::async([] {
       glz::server server{8080};
 
-     const auto ec = server.async_accept([](glz::socket&& client) {
-          std::cout << "New client connected!\n";
+      const auto ec = server.async_accept([](glz::socket&& client) {
+         std::cout << "New client connected!\n";
 
-          client.read([](const std::string& data, int bytes_read) {
-              std::string received(data.begin(), data.begin() + bytes_read);
-              std::cout << "Received from client: " << received << std::endl;
-          });
+         client.read([](const std::string& data, int bytes_read) {
+            std::string received(data.begin(), data.begin() + bytes_read);
+            std::cout << "Received from client: " << received << std::endl;
+         });
 
          std::string message = "Welcome!";
-          client.write(message, [](const std::string& data, int /*bytes_sent*/) {
-             std::cout << std::format("Sent to client: {}\n", data);
-          });
+         client.write(message, [](const std::string& data, int /*bytes_sent*/) {
+            std::cout << std::format("Sent to client: {}\n", data);
+         });
       });
-      
+
       if (ec) {
          std::cerr << ec.message() << '\n';
       }
       else {
          std::cout << std::format("Server started on port: {}\n", server.port);
       }
-      
-       // Keep thread alive
-       std::this_thread::sleep_for(std::chrono::hours(1));
+
+      // Keep thread alive
+      std::this_thread::sleep_for(std::chrono::hours(1));
    });
-   
+
    // Allow the socket to get started
    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 };
@@ -52,23 +51,24 @@ suite make_server = [] {
 suite socket_test = [] {
    glz::socket socket{};
 
-    if (socket.connect("127.0.0.1", 8080)) {
-       std::cerr << "Failed to connect to server.\n";
-    } else {
-       std::cout << "Connected to server!\n";
+   if (socket.connect("127.0.0.1", 8080)) {
+      std::cerr << "Failed to connect to server.\n";
+   }
+   else {
+      std::cout << "Connected to server!\n";
 
-       socket.read([](const std::string& data, int bytes_read) {
-           std::string received(data.begin(), data.begin() + bytes_read);
-           std::cout << "Received: " << received << std::endl;
-       });
+      socket.read([](const std::string& data, int bytes_read) {
+         std::string received(data.begin(), data.begin() + bytes_read);
+         std::cout << "Received: " << received << std::endl;
+      });
 
       std::string message = "Hello World";
-       socket.write(message, [](const std::string& data, int bytes_sent) {
-           std::cout << "Sent from client: " << std::string(data.begin(), data.begin() + bytes_sent) << std::endl;
-       });
-    }
+      socket.write(message, [](const std::string& data, int bytes_sent) {
+         std::cout << "Sent from client: " << std::string(data.begin(), data.begin() + bytes_sent) << std::endl;
+      });
+   }
 
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+   std::this_thread::sleep_for(std::chrono::seconds(10));
 };
 
 int main() { return 0; }
