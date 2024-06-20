bfd05dc329a1e9bcf792b7c645b6718fd0e4e339
stephenberry <stephenberry@users.noreply.github.com>
2024-06-20 13:17:45 +0000

    Committing clang-format changes

--- include/glaze/json/json_concepts.hpp
+++ include/glaze/json/json_concepts.hpp
@@ -8,23 +8,24 @@
 namespace glz
 {
    template <class T>
-   concept json_object = detail::glaze_object_t<T> || detail::reflectable<T> || detail::writable_map_t<T> || detail::readable_map_t<T>;
-   
+   concept json_object =
+      detail::glaze_object_t<T> || detail::reflectable<T> || detail::writable_map_t<T> || detail::readable_map_t<T>;
+
    template <class T>
    concept json_array = detail::array_t<T>;
-   
+
    template <class T>
    concept json_string = detail::str_t<T>;
-   
+
    template <class T>
    concept json_boolean = detail::boolean_like<T>;
-   
+
    template <class T>
    concept json_number = detail::num_t<T>;
-   
+
    template <class T>
    concept json_integer = detail::int_t<T>;
-   
+
    template <class T>
    concept json_null = detail::null_t<T>;
 }
