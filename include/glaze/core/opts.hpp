fec8a67a42bf666dfb304d55fa274cc8611e6f26
Stephen Berry <stephenberry.developer@gmail.com>
2024-06-29 07:55:12 -0500

    Revert "conversions"
    This reverts commit 5046f1a70ad75fa55238dbb2f375ec9e48a71b96.

--- include/glaze/core/opts.hpp
+++ include/glaze/core/opts.hpp
@@ -29,11 +29,7 @@ namespace glz
    // Write padding bytes simplifies our dump calculations by making sure we have significant excess
    constexpr uint32_t write_padding_bytes = 256;
    
-   struct opts;
-   
-   struct alignas(64) opts_packed {
-      opts unpack() const;
-   };
+   struct alignas(64) opts_packed {};
 
    struct alignas(opts_packed) opts
    {
@@ -99,10 +95,6 @@ namespace glz
          return std::bit_cast<opts_packed>(*this);
       }
    };
-   
-   inline opts opts_packed::unpack() const {
-      return std::bit_cast<opts>(*this);
-   }
    
    static_assert(sizeof(opts) == sizeof(opts_packed));
 
