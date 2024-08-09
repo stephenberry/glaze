ffed66b0e483a53ecdfbb644c1b89173743242ff
Stephen Berry <stephenberry.developer@gmail.com>
2024-08-09 12:55:37 -0500

    Using hash map again for internal enums

--- include/glaze/reflection/enum_macro.hpp
+++ include/glaze/reflection/enum_macro.hpp
@@ -35,7 +35,7 @@ namespace glz
    macro(a1, a2) __VA_OPT__(, GLZ_FOR_EACH_AGAIN2 GLZ_PARENS(macro, __VA_ARGS__))
 #define GLZ_FOR_EACH_AGAIN2() GLZ_FOR_EACH_HELPER2
 
-#define GLZ_EXTRACT_FIRST(x, y) x
+#define GLZ_EXTRACT_FIRST(x, y) std::string_view{ x }
 #define GLZ_EXTRACT_SECOND(x, y) y
 
 // Create an enum with the provided fields. A nameof reflection function made.
@@ -43,8 +43,8 @@ namespace glz
 // nameof(color::red) == "red"
 #define GLZ_ENUM(EnumType, ...)                                                                              \
    enum struct EnumType : uint32_t { __VA_ARGS__ };                                                          \
-   constexpr auto EnumType##_names = std::array{GLZ_FOR_EACH(GLZ_STRINGIFY, __VA_ARGS__)};                   \
-   constexpr std::string_view nameof(EnumType value) noexcept { return (EnumType##_names)[size_t(value)]; }; \
+   inline constexpr auto EnumType##_names = std::array{GLZ_FOR_EACH(GLZ_STRINGIFY, __VA_ARGS__)};                   \
+   inline constexpr std::string_view nameof(EnumType value) noexcept { return (EnumType##_names)[size_t(value)]; }; \
    template <class T>                                                                                        \
       requires(std::same_as<std::decay_t<T>, EnumType>)                                                      \
    constexpr decltype(auto) enum_names(T&&) noexcept                                                         \
@@ -57,8 +57,8 @@ namespace glz
 // nameof(color::red) == "Red"
 #define GLZ_ENUM_MAP(EnumType, ...)                                                                          \
    enum struct EnumType : uint32_t { GLZ_FOR_EACH2(GLZ_EXTRACT_SECOND, __VA_ARGS__) };                       \
-   constexpr auto EnumType##_names = std::array{GLZ_FOR_EACH2(GLZ_EXTRACT_FIRST, __VA_ARGS__)};              \
-   constexpr std::string_view nameof(EnumType value) noexcept { return (EnumType##_names)[size_t(value)]; }; \
+   inline constexpr auto EnumType##_names = std::array{GLZ_FOR_EACH2(GLZ_EXTRACT_FIRST, __VA_ARGS__)};              \
+   inline constexpr std::string_view nameof(EnumType value) noexcept { return (EnumType##_names)[size_t(value)]; }; \
    template <class T>                                                                                        \
       requires(std::same_as<std::decay_t<T>, EnumType>)                                                      \
    constexpr decltype(auto) enum_names(T&&) noexcept                                                         \
