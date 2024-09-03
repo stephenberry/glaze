bb30bdafe61afb417fcfd142bbd47f77832dccef
stephenberry <stephenberry@users.noreply.github.com>
2024-09-03 11:36:37 +0000

    Committing clang-format changes

--- include/glaze/file/read_directory.hpp
+++ include/glaze/file/read_directory.hpp
@@ -14,16 +14,18 @@ namespace glz
    {
       // TODO: When Apple Clang supports hashing on std::filesystem::path with precompiled headers, we can remove this
       // std::hash<std::filesystem::path> not in the C++20 standard by default
-      struct fs_path_hash {
-         [[nodiscard]] auto operator()(const std::filesystem::path& path) const noexcept {
+      struct fs_path_hash
+      {
+         [[nodiscard]] auto operator()(const std::filesystem::path& path) const noexcept
+         {
             return std::filesystem::hash_value(path);
          }
       };
    }
-   
+
    // files should be a map of std::filesystem::path to std::string buffers
-   [[nodiscard]] inline error_ctx directory_to_buffers(auto& files,
-                                                       const sv directory_path, const sv target_extension = ".json")
+   [[nodiscard]] inline error_ctx directory_to_buffers(auto& files, const sv directory_path,
+                                                       const sv target_extension = ".json")
    {
       for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
          if (entry.is_regular_file() && (target_extension.empty() || (entry.path().extension() == target_extension))) {
