16d05ef5545555fd9567708580c7df58be8c3492
Stephen Berry <stephenberry.developer@gmail.com>
2024-09-03 06:36:10 -0500

    Fix Apple clang precompiled headers due to path hashing (#1284)
    * Fix Apple clang precompiled headers due to path hashing
    
    * Fix namespace

--- include/glaze/file/read_directory.hpp
+++ include/glaze/file/read_directory.hpp
@@ -10,7 +10,19 @@
 
 namespace glz
 {
-   [[nodiscard]] inline error_ctx directory_to_buffers(std::unordered_map<std::filesystem::path, std::string>& files,
+   namespace detail
+   {
+      // TODO: When Apple Clang supports hashing on std::filesystem::path with precompiled headers, we can remove this
+      // std::hash<std::filesystem::path> not in the C++20 standard by default
+      struct fs_path_hash {
+         [[nodiscard]] auto operator()(const std::filesystem::path& path) const noexcept {
+            return std::filesystem::hash_value(path);
+         }
+      };
+   }
+   
+   // files should be a map of std::filesystem::path to std::string buffers
+   [[nodiscard]] inline error_ctx directory_to_buffers(auto& files,
                                                        const sv directory_path, const sv target_extension = ".json")
    {
       for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
@@ -26,7 +38,7 @@ namespace glz
    template <opts Opts = opts{}, detail::readable_map_t T>
    [[nodiscard]] error_ctx read_directory(T& value, const sv directory_path, const sv target_extension = ".json")
    {
-      std::unordered_map<std::filesystem::path, std::string> files{};
+      std::unordered_map<std::filesystem::path, std::string, detail::fs_path_hash> files{};
       if (auto ec = directory_to_buffers(files, directory_path, target_extension)) {
          return ec;
       }
