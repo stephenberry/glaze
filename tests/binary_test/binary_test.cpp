31e0c15b1eb2c741ebc570cf98e8cb6010dde278
Stephen Berry <stephenberry.developer@gmail.com>
2024-09-18 14:21:52 -0400

    Another test

--- tests/binary_test/binary_test.cpp
+++ tests/binary_test/binary_test.cpp
@@ -2404,6 +2404,17 @@ suite past_fuzzing_issues = [] {
       expect(glz::read_binary<my_struct>(input).error());
       expect(glz::beve_to_json(input, json));
    };
+   
+   "fuzz13"_test = [] {
+      std::string_view base64 = "AzAAYQ==";
+      std::vector<uint8_t> input = base64_decode(base64);
+      expect(glz::read_binary<my_struct>(input).error());
+      std::string json{};
+      expect(glz::beve_to_json(input, json));
+      input.push_back('\0');
+      expect(glz::read_binary<my_struct>(input).error());
+      expect(glz::beve_to_json(input, json));
+   };
 };
 
 int main()
