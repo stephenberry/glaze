21ab9e31e3be14b240c83ecccf445e3e8021f162
Stephen Berry <stephenberry.developer@gmail.com>
2024-09-23 09:02:58 -0500

    Update check-bmi2.cmake

--- cmake/check-bmi2.cmake
+++ cmake/check-bmi2.cmake
@@ -1,24 +1,49 @@
 include(CheckCXXSourceCompiles)
+include(CheckCXXCompilerFlag)
 
 set(GLZ_HAS_BMI2 FALSE)
 
-# Set the required compiler flags for BMI2 detection
-if(MSVC)
-    set(CMAKE_REQUIRED_FLAGS "/arch:AVX2")
-else()
-    set(CMAKE_REQUIRED_FLAGS "-mbmi2")
+set(BMI2_FLAG "")
+
+if (MSVC)
+    set(BMI2_FLAG "/arch:AVX2")
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR
+        CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(BMI2_FLAG "-mbmi2")
 endif()
 
-# BMI2 detection
-check_cxx_source_compiles("
-#include <immintrin.h>
-int main() {
-    unsigned long long src = 0xFF, mask = 0xF0;
-    unsigned long long result = _pext_u64(src, mask);
-    (void)result;
-    return 0;
-}
-" GLZ_HAS_BMI2)
+# Check if the compiler supports the BMI2 flag
+if (BMI2_FLAG)
+    check_cxx_compiler_flag("${BMI2_FLAG}" COMPILER_SUPPORTS_BMI2_FLAG)
+    if (COMPILER_SUPPORTS_BMI2_FLAG)
+        # Append BMI2 flag to CMAKE_REQUIRED_FLAGS without overwriting existing flags
+        set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${BMI2_FLAG}")
+    else()
+        message(STATUS "BMI2 instructions: not supported and not used.")
+    endif()
+endif()
+
+if (BMI2_FLAG)
+    # BMI2 detection
+    check_cxx_source_compiles("
+    #include <immintrin.h>
+    int main() {
+        unsigned long long src = 0xFF, mask = 0xF0;
+        unsigned long long result = _pext_u64(src, mask);
+        (void)result;
+        return 0;
+    }
+    " GLZ_HAS_BMI2)
+
+    string(REGEX REPLACE "${BMI2_FLAG}" "" CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+endif()
+
+# Reset CMAKE_REQUIRED_FLAGS to avoid affecting other checks
+if(MSVC)
+string(REGEX REPLACE "/arch:AVX2" "" CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+else()
+string(REGEX REPLACE "-mbmi2" "" CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+endif()
 
 if(GLZ_HAS_BMI2)
     message(STATUS "BMI2 instructions are supported.")
