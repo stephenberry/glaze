6f7ff56e3a57f4336a700d3a59034a69cfb557d5
Stephen Berry <stephenberry.developer@gmail.com>
2024-09-25 16:46:32 -0500

    Update feature test macros

--- include/glaze/core/feature_test.hpp
+++ include/glaze/core/feature_test.hpp
@@ -4,9 +4,5 @@
 #pragma once
 
 // Feature test macros
-#define glaze_v3_6_0_refl // Version 3.6.0 reflect metadata
-
-namespace glz::features
-{
-   static constexpr auto v3_refl = true;
-}
+#define glaze_v3_5_to_from // v3.5.0 specialized to/from
+#define glaze_v3_6_reflect // v3.6.0 reflect metadata
