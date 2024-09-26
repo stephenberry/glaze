6d249bf40f926571c54fd16f3c4ebb3b12f8060e
Stephen Berry <stephenberry.developer@gmail.com>
2024-09-25 21:37:45 -0500

    Update feature_test.hpp

--- include/glaze/core/feature_test.hpp
+++ include/glaze/core/feature_test.hpp
@@ -3,13 +3,13 @@
 
 #pragma once
 
-// Feature test macros
+// Glaze Feature Test Macros
+
+// v3.6.0 renames glz::refl to glz::reflect and does not instantiate the struct as an
+// inline constexpr variable
+#define glaze_v3_6_0_reflect
 
 // v3.5.0 change glz::detail::to_json and glz::detail::from_json specializations
 // to glz::detail::to<JSON and glz::detail::from<JSON
 // The template specialization take in a uint32_t Format as the first template parameter
-#define glaze_v3_5_to_from
-
-// v3.6.0 renames glz::refl to glz::reflect and does not instantiate the struct as an
-// inline constexpr variable
-#define glaze_v3_6_reflect
+#define glaze_v3_5_0_to_from
