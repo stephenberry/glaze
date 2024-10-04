91565a7882b30be1c82e2a5750c4f8304f5431b4
stephenberry <stephenberry@users.noreply.github.com>
2024-10-04 18:08:36 +0000

    Committing clang-format changes

--- tests/json_test/json_test.cpp
+++ tests/json_test/json_test.cpp
@@ -9663,14 +9663,17 @@ suite flat_map_tests = [] {
    };
 };
 
-struct Foo  {
+struct Foo
+{
    int x;
 };
 
 suite ndjson_options = [] {
    "ndjson_options"_test = [] {
       std::vector<Foo> assets{};
-      const auto ec = glz::read<glz::opts{.format = glz::NDJSON, .error_on_unknown_keys = false, .validate_skipped = true}>(assets, "{\"x\":1}\n{\"x\":2}");
+      const auto ec =
+         glz::read<glz::opts{.format = glz::NDJSON, .error_on_unknown_keys = false, .validate_skipped = true}>(
+            assets, "{\"x\":1}\n{\"x\":2}");
       expect(not ec);
    };
 };
