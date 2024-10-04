dafdca860970e28906f69a6ae95fbe9161ba39b2
Stephen Berry <stephenberry.developer@gmail.com>
2024-10-04 12:51:35 -0500

    Int testing

--- tests/json_performance/json_performance.cpp
+++ tests/json_performance/json_performance.cpp
@@ -195,15 +195,42 @@ suite integers_test = [] {
       std::string buffer;
       auto t0 = std::chrono::steady_clock::now();
       glz::error_ctx e;
-      for (uint32_t i = 0; i < n; ++i) {
+      for (size_t i = 0; i < n; ++i) {
          v.a = int32_t(i);
          v.b = uint32_t(i);
          v.c = int64_t(i);
          v.d = uint64_t(i);
          std::ignore = glz::write_json(v, buffer);
          e = glz::read_json(v, buffer);
-         std::memcpy(&v, &i, sizeof(float));
       }
+      auto t1 = std::chrono::steady_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() * 1e-6;
+      std::cout << "integers read/write: " << duration << '\n';
+   };
+};
+
+suite uint64_t_test = [] {
+   "uint64_t"_test = [] {
+      SKIP;
+
+#ifdef NDEBUG
+      constexpr size_t n = 100000000;
+#else
+      constexpr size_t n = 100000;
+#endif
+      
+      std::string buffer;
+      auto t0 = std::chrono::steady_clock::now();
+      glz::error_ctx e;
+      for (size_t i = 0; i < n; ++i) {
+         auto v = uint64_t(i);
+         std::ignore = glz::write_json(v, buffer);
+         e = glz::read_json(v, buffer);
+         if (bool(e)) {
+            break;
+         }
+      }
+      expect(not e);
       auto t1 = std::chrono::steady_clock::now();
       auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() * 1e-6;
       std::cout << "integers read/write: " << duration << '\n';
