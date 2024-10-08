682890519f4b284958b1544737855dd110dab262
Stephen Berry <stephenberry.developer@gmail.com>
2024-10-08 18:47:33 -0500

    New integer parsing (#1362)
    * New integer parsing
    
    * Update jsonrpc_test.cpp
    
    * Maybe fix GCC
    
    * Removed old code
    
    * Add parse_ints_as_type_cast_doubles
    
    * full_multiplication
    
    * Update opts.hpp
    
    * Fix MSVC build
    
    * Fixed i64 full_multiplication
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Stephen Berry <stephen.berry@matrixresearch.com>

--- include/glaze/util/parse.hpp
+++ include/glaze/util/parse.hpp
@@ -15,7 +15,7 @@
 #include "glaze/util/convert.hpp"
 #include "glaze/util/expected.hpp"
 #include "glaze/util/inline.hpp"
-#include "glaze/util/stoui64.hpp"
+#include "glaze/util/atoi.hpp"
 #include "glaze/util/string_literal.hpp"
 
 namespace glz::detail
