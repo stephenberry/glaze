f5332d3f8e328c75c875622b9ef6ac79782d1056
Stephen Berry <stephenberry.developer@gmail.com>
2024-10-09 06:30:58 -0500

    Fixing merge

--- include/glaze/json/read.hpp
+++ include/glaze/json/read.hpp
@@ -1028,46 +1028,33 @@ namespace glz
             }
 
             auto start = it;
-            
             skip_string_view<Opts>(ctx, it, end);
             if (bool(ctx.error)) [[unlikely]]
                return;
 
             if constexpr (string_view_t<T>) {
                value = {start, size_t(it - start)};
-               ++it; // skip closing quote
-               GLZ_VALID_END();
             }
             else if constexpr (char_array_t<T>) {
-               skip_string_view<Opts>(ctx, it, end);
-               if (bool(ctx.error)) [[unlikely]]
-                  return;
-
                const size_t n = it - start;
-               if ((sizeof(value) - 1) < n) {
+               if ((n + 1) > sizeof(value)) {
                   ctx.error = error_code::unexpected_end;
                   return;
                }
                std::memcpy(value, start, n);
                value[n] = '\0';
-               ++it; // skip closing quote
-               GLZ_VALID_END();
             }
             else if constexpr (array_char_t<T>) {
-               skip_string_view<Opts>(ctx, it, end);
-               if (bool(ctx.error)) [[unlikely]]
-                  return;
-
                const size_t n = it - start;
-               if ((sizeof(value) - 1) < n) {
+               if ((n + 1) > value.size()) {
                   ctx.error = error_code::unexpected_end;
                   return;
                }
-               std::memcpy(value, start, n);
+               std::memcpy(value.data(), start, n);
                value[n] = '\0';
-               ++it; // skip closing quote
-               GLZ_VALID_END();
             }
+            ++it; // skip closing quote
+            GLZ_VALID_END();
          }
       };
 
