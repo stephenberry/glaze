bada6f4cd52611f5ec8f8bcc15e4db3ea7aabe14
Stephen Berry <stephenberry.developer@gmail.com>
2024-10-28 07:38:14 -0500

    json_t assignment operators and string_view constructor

--- include/glaze/json/json_t.hpp
+++ include/glaze/json/json_t.hpp
@@ -90,6 +90,26 @@ namespace glz
          }
          return iter->second;
       }
+      
+      json_t& operator=(const std::string& value) {
+         data = value;
+         return *this;
+      }
+      
+      json_t& operator=(const std::string_view value) {
+         data = std::string(value);
+         return *this;
+      }
+      
+      json_t& operator=(const double value) {
+         data = value;
+         return *this;
+      }
+      
+      json_t& operator=(const bool value) {
+         data = value;
+         return *this;
+      }
 
       [[nodiscard]] json_t& at(std::convertible_to<std::string_view> auto&& key) { return operator[](key); }
 
@@ -132,6 +152,11 @@ namespace glz
       json_t(T&& val)
       {
          data = static_cast<double>(val);
+      }
+      
+      json_t(const std::string_view value)
+      {
+         data = std::string(value);
       }
 
       json_t(std::initializer_list<std::pair<const char*, json_t>>&& obj)
