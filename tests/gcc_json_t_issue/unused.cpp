326d39dce89350962ce228795141e42d44f6b380
stephenberry <stephenberry@users.noreply.github.com>
2024-12-18 13:53:52 +0000

    Committing clang-format changes

--- tests/gcc_json_t_issue/unused.cpp
+++ tests/gcc_json_t_issue/unused.cpp
@@ -1,5 +1,3 @@
 #include <glaze/json.hpp>
 
-[[maybe_unused]] static bool unused_function() {
-    return glz::json_t{}.dump().has_value();
-}
+[[maybe_unused]] static bool unused_function() { return glz::json_t{}.dump().has_value(); }
