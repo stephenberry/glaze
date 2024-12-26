254c7be7222472d4d58c30fbb7a1a9c841a562e4
stephenberry <stephenberry@users.noreply.github.com>
2024-12-26 20:23:29 +0000

    Committing clang-format changes

--- include/glaze/json/read.hpp
+++ include/glaze/json/read.hpp
@@ -1784,7 +1784,7 @@ namespace glz
             GLZ_SKIP_WS();
             const size_t ws_size = size_t(it - ws_start);
 
-            if constexpr ((glaze_object_t<T> || reflectable<T>) && num_members == 0 && Opts.error_on_unknown_keys) {
+            if constexpr ((glaze_object_t<T> || reflectable<T>)&&num_members == 0 && Opts.error_on_unknown_keys) {
                if (*it == '}') [[likely]] {
                   GLZ_SUB_LEVEL;
                   ++it;
@@ -1799,8 +1799,8 @@ namespace glz
             }
             else {
                decltype(auto) fields = [&]() -> decltype(auto) {
-                  if constexpr ((glaze_object_t<T> || reflectable<T>) &&
-                                (Opts.error_on_missing_keys || Opts.partial_read)) {
+                  if constexpr ((glaze_object_t<T> || reflectable<T>)&&(Opts.error_on_missing_keys ||
+                                                                        Opts.partial_read)) {
                      return bit_array<num_members>{};
                   }
                   else {
@@ -1812,7 +1812,7 @@ namespace glz
 
                bool first = true;
                while (true) {
-                  if constexpr ((glaze_object_t<T> || reflectable<T>) && Opts.partial_read) {
+                  if constexpr ((glaze_object_t<T> || reflectable<T>)&&Opts.partial_read) {
                      static constexpr bit_array<num_members> all_fields = [] {
                         bit_array<num_members> arr{};
                         for (size_t i = 0; i < num_members; ++i) {
@@ -1829,14 +1829,14 @@ namespace glz
 
                   if (*it == '}') {
                      GLZ_SUB_LEVEL;
-                     if constexpr ((glaze_object_t<T> || reflectable<T>) &&
-                                   (Opts.partial_read && Opts.error_on_missing_keys)) {
+                     if constexpr ((glaze_object_t<T> ||
+                                    reflectable<T>)&&(Opts.partial_read && Opts.error_on_missing_keys)) {
                         ctx.error = error_code::missing_key;
                         return;
                      }
                      else {
                         ++it;
-                        if constexpr ((glaze_object_t<T> || reflectable<T>) && Opts.error_on_missing_keys) {
+                        if constexpr ((glaze_object_t<T> || reflectable<T>)&&Opts.error_on_missing_keys) {
                            constexpr auto req_fields = required_fields<T, Opts>();
                            if ((req_fields & fields) != req_fields) {
                               ctx.error = error_code::missing_key;
--- tests/json_test/json_test.cpp
+++ tests/json_test/json_test.cpp
@@ -346,7 +346,7 @@ struct Thing
    std::map<int, double> mapi{{5, 3.14}, {7, 7.42}, {2, 9.63}};
    sub_thing* thing_ptr{};
 
-   Thing() : thing_ptr(&thing) {};
+   Thing() : thing_ptr(&thing){};
 };
 
 template <>
@@ -4197,7 +4197,7 @@ struct glz::meta<cat>
 
 struct person
 {
-   void eat(const std::string&) {};
+   void eat(const std::string&){};
 };
 
 template <>
@@ -6458,8 +6458,9 @@ template <>
 struct glz::meta<test_mapping_t>
 {
    using T = test_mapping_t;
-   static constexpr auto value =
-      object("id", &T::id, "coordinates", [](auto& self) { return coordinates_t{&self.latitude, &self.longitude}; });
+   static constexpr auto value = object("id", &T::id, "coordinates", [](auto& self) {
+      return coordinates_t{&self.latitude, &self.longitude};
+   });
 };
 
 suite mapping_struct = [] {
@@ -9737,8 +9738,8 @@ template <class V>
 struct glz::meta<response_t<V>>
 {
    using T = response_t<V>;
-   static constexpr auto value =
-      object("result", [](auto& s) -> auto& { return s.result; }, "id", &T::id, "error", &T::error);
+   static constexpr auto value = object(
+      "result", [](auto& s) -> auto& { return s.result; }, "id", &T::id, "error", &T::error);
 };
 
 template <>
@@ -9957,7 +9958,7 @@ namespace trr
 
    struct Person
    {
-      Person(Address* const p_add) : p_add(p_add) {};
+      Person(Address* const p_add) : p_add(p_add){};
       std::string name;
       Address* const p_add; // pointer is const, Address object is mutable
    };
