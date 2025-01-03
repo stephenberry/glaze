9ec37aa89e3a38ca6e8d67fcb6e6b076f336c729
Stephen Berry <stephenberry.developer@gmail.com>
2025-01-03 12:50:19 -0600

    Remove disabled option

--- include/glaze/trace/trace.hpp
+++ include/glaze/trace/trace.hpp
@@ -62,16 +62,12 @@ namespace glz
 
       std::optional<std::chrono::time_point<std::chrono::steady_clock>> t0{}; // the time of the first event
 
-      std::atomic<bool> disabled = false;
       std::mutex mtx{};
 
       template <class... Args>
          requires(sizeof...(Args) <= 1)
       void begin(const std::string_view name, Args&&... args) noexcept
       {
-         if (disabled) {
-            return;
-         }
          duration(name, 'B', std::forward<Args>(args)...);
       }
 
@@ -79,9 +75,6 @@ namespace glz
          requires(sizeof...(Args) <= 1)
       void end(const std::string_view name, Args&&... args) noexcept
       {
-         if (disabled) {
-            return;
-         }
          duration(name, 'E', std::forward<Args>(args)...);
       }
 
@@ -89,10 +82,6 @@ namespace glz
          requires(sizeof...(Args) <= 1)
       void duration(const std::string_view name, const char phase, Args&&... args) noexcept
       {
-         if (disabled) {
-            return;
-         }
-
          const auto tnow = std::chrono::steady_clock::now();
          trace_event* event{};
          {
@@ -115,9 +104,6 @@ namespace glz
          requires(sizeof...(Args) <= 1)
       void async_begin(const std::string_view name, Args&&... args) noexcept
       {
-         if (disabled) {
-            return;
-         }
          async(name, 'b', std::forward<Args>(args)...);
       }
 
@@ -125,9 +111,6 @@ namespace glz
          requires(sizeof...(Args) <= 1)
       void async_end(const std::string_view name, Args&&... args) noexcept
       {
-         if (disabled) {
-            return;
-         }
          async(name, 'e', std::forward<Args>(args)...);
       }
 
@@ -135,10 +118,6 @@ namespace glz
          requires(sizeof...(Args) <= 1)
       void async(const std::string_view name, const char phase, Args&&... args) noexcept
       {
-         if (disabled) {
-            return;
-         }
-
          const auto tnow = std::chrono::steady_clock::now();
          trace_event* event{};
          {
