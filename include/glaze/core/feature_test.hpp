dc20b1985ea7fa360a108e09d1b72064bc93a7dc
Stephen Berry <stephenberry.developer@gmail.com>
2025-01-31 09:26:34 -0600

    Update feature_test.hpp (#1582)

--- include/glaze/core/feature_test.hpp
+++ include/glaze/core/feature_test.hpp
@@ -5,11 +5,17 @@
 
 // Glaze Feature Test Macros
 
+// v4.3.0 removed global glz::trace
+#define glaze_v4_3_0_trace
+
+// v4.2.3 renamed glz::tuplet::tuple to glz::tuple
+#define glaze_v4_2_3_tuple
+
 // v3.6.0 renames glz::refl to glz::reflect and does not instantiate the struct as an
 // inline constexpr variable
 #define glaze_v3_6_0_reflect
 
 // v3.5.0 change glz::detail::to_json and glz::detail::from_json specializations
 // to glz::detail::to<JSON and glz::detail::from<JSON
-// The template specialization take in a uint32_t Format as the first template parameter
+// The template specialization takes a uint32_t Format as the first template parameter
 #define glaze_v3_5_0_to_from
