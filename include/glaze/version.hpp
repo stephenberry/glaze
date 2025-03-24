eaa612972fbad6bfa46155528065c1b7bc5d30f9
stephenberry <stephenberry@users.noreply.github.com>
2025-03-24 20:34:05 +0000

    Committing clang-format changes

--- include/glaze/version.hpp
+++ include/glaze/version.hpp
@@ -16,24 +16,22 @@ namespace glz
       uint8_t major = 5;
       uint8_t minor = 0;
       uint8_t patch = 1;
-      
+
       constexpr auto operator<=>(const version_t& other) const noexcept
       {
          // Compare major versions first
-         if (auto cmp = major <=> other.major; cmp != 0)
-            return cmp;
+         if (auto cmp = major <=> other.major; cmp != 0) return cmp;
          // If major versions are equal, compare minor versions
-         if (auto cmp = minor <=> other.minor; cmp != 0)
-            return cmp;
+         if (auto cmp = minor <=> other.minor; cmp != 0) return cmp;
          // If major and minor versions are equal, compare patch versions
          return patch <=> other.patch;
       }
-      
+
       // In C++23, this is optional when we have a custom <=> operator,
       // but included for clarity
       constexpr bool operator==(const version_t& other) const noexcept = default;
    };
-   
+
    /**
     * @var version
     * @brief Global constant instance of the current library version
