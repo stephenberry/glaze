7be99119ba7500c647867dba94a78cadc6986d0a
Stephen Berry <stephenberry.developer@gmail.com>
2025-04-29 11:37:31 -0500

    version 5.1.1 bump

--- include/glaze/version.hpp
+++ include/glaze/version.hpp
@@ -15,7 +15,7 @@ namespace glz
    {
       uint8_t major = 5;
       uint8_t minor = 1;
-      uint8_t patch = 0;
+      uint8_t patch = 1;
 
       constexpr auto operator<=>(const version_t& other) const noexcept = default;
 
