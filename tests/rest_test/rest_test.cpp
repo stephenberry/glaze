c4660f348c0a3daa6cb1d39dc4127446b4472d75
Stephen Berry <stephenberry.developer@gmail.com>
2025-05-28 12:25:34 -0500

    renamed to http_client and http_server

--- tests/rest_test/rest_test.cpp
+++ tests/rest_test/rest_test.cpp
@@ -2,7 +2,7 @@
 // For the license information refer to glaze.hpp
 
 #include "glaze/glaze.hpp"
-#include "glaze/rest/rest_server.hpp"
+#include "glaze/rest/http_server.hpp"
 #include "ut/ut.hpp"
 
 #include <iostream>
