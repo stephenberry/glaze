0195f90d260d7796f986cd11f773f5f20ce1c070
Stephen Berry <stephenberry.developer@gmail.com>
2025-05-28 12:52:35 -0500

    Update rest_server.cpp

--- tests/rest_test/rest_server/rest_server.cpp
+++ tests/rest_test/rest_server/rest_server.cpp
@@ -278,8 +278,8 @@ int main()
 
    // Start the server
    server.bind("127.0.0.1", 8080);
-   std::cout << "🚀 Glaze Demo Server running on http://127.0.0.1:8080\n";
-   std::cout << "📁 Make sure index.html is in the same directory\n";
+   std::cout << "Glaze Demo Server running on http://127.0.0.1:8080\n";
+   std::cout << "Make sure index.html is in the same directory\n";
    std::cout << "🛑 Press Enter to stop the server...\n\n";
 
    server.start();
