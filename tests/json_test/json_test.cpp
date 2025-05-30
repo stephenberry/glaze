327f21e0f41de2bff6ab64f72ac14eb1cc6b7aa3
Stephen Berry <stephenberry.developer@gmail.com>
2025-05-30 05:58:58 -0500

    Report the first missing key in format_error

--- tests/json_test/json_test.cpp
+++ tests/json_test/json_test.cpp
@@ -6059,8 +6059,22 @@ suite required_keys = [] {
       my_struct obj{};
       std::string buffer = R"({"i":287,"hello":"Hello World","arr":[1,2,3]})";
       auto err = glz::read<glz::opts{.error_on_missing_keys = true}>(obj, buffer);
-      expect(err != glz::error_code::none);
-      expect(glz::format_error(err, buffer) == "index 45: missing_key") << glz::format_error(err, buffer);
+      expect(bool(err));
+      auto err_msg = glz::format_error(err, buffer);
+      expect(err_msg == "index 45: missing_key d") << err_msg;
+      
+      buffer = R"({"i":287,"d":0.0,"arr":[1,2,3]})";
+      err = glz::read<glz::opts{.error_on_missing_keys = true}>(obj, buffer);
+      expect(bool(err));
+      err_msg = glz::format_error(err, buffer);
+      expect(err_msg == "index 31: missing_key hello") << err_msg;
+      
+      std::vector<my_struct> vec{};
+      buffer = R"([{"i":287,"d":0.0,"arr":[1,2,3]}])";
+      err = glz::read<glz::opts{.error_on_missing_keys = true}>(vec, buffer);
+      expect(bool(err));
+      err_msg = glz::format_error(err, buffer);
+      expect(err_msg == "1:33: missing_key\n   [{\"i\":287,\"d\":0.0,\"arr\":[1,2,3]}]\n                                   ^ hello") << err_msg;
    };
 };
 
