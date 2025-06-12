126e918f78e92e2e61fc6ff720446e4c3931dd07
Stephen Berry <stephenberry.developer@gmail.com>
2025-06-11 19:06:04 -0500

    Update http_client.hpp

--- include/glaze/net/http_client.hpp
+++ include/glaze/net/http_client.hpp
@@ -186,43 +186,54 @@ namespace glz
    struct http_client
    {
       http_client()
-         : io_context(std::make_shared<asio::io_context>()),
-           connection_pool(std::make_shared<http_connection_pool>(io_context))
+         : async_io_context(std::make_shared<asio::io_context>()),
+           connection_pool(std::make_shared<http_connection_pool>(async_io_context))
       {
-         // Start worker threads
+         // Start worker threads for async operations only
          start_workers();
       }
 
       ~http_client() { stop_workers(); }
 
-      // Synchronous GET request
+      // Synchronous GET request - truly synchronous, no promises/futures
       std::expected<response, std::error_code> get(std::string_view url,
                                                    const std::unordered_map<std::string, std::string>& headers = {})
       {
-         std::promise<std::expected<response, std::error_code>> promise;
-         auto future = promise.get_future();
+         auto url_result = parse_url(url);
+         if (!url_result) {
+            return std::unexpected(url_result.error());
+         }
 
-         get_async(url, headers,
-                   [promise = std::move(promise)](std::expected<response, std::error_code> result) mutable {
-                      promise.set_value(std::move(result));
-                   });
-
-         return future.get();
+         return perform_sync_request("GET", *url_result, "", headers);
       }
 
-      // Synchronous POST request
+      // Synchronous POST request - truly synchronous, no promises/futures
       std::expected<response, std::error_code> post(std::string_view url, std::string_view body,
                                                     const std::unordered_map<std::string, std::string>& headers = {})
       {
-         std::promise<std::expected<response, std::error_code>> promise;
-         auto future = promise.get_future();
+         auto url_result = parse_url(url);
+         if (!url_result) {
+            return std::unexpected(url_result.error());
+         }
 
-         post_async(url, body, headers,
-                    [promise = std::move(promise)](std::expected<response, std::error_code> result) mutable {
-                       promise.set_value(std::move(result));
-                    });
+         return perform_sync_request("POST", *url_result, std::string(body), headers);
+      }
 
-         return future.get();
+      // Synchronous JSON POST request
+      template <class T>
+      std::expected<response, std::error_code> post_json(
+         std::string_view url, const T& data, const std::unordered_map<std::string, std::string>& headers = {})
+      {
+         std::string json_str;
+         auto ec = glz::write_json(data, json_str);
+         if (ec) {
+            return std::unexpected(std::make_error_code(std::errc::invalid_argument));
+         }
+
+         auto merged_headers = headers;
+         merged_headers["Content-Type"] = "application/json";
+
+         return post(url, json_str, merged_headers);
       }
 
       // Asynchronous GET request
@@ -232,9 +243,8 @@ namespace glz
       {
          auto url_result = parse_url(url);
          if (!url_result) {
-            asio::post(*io_context, [handler = std::forward<CompletionHandler>(handler), error = url_result.error()] mutable {
-               handler(std::unexpected(error));
-            });
+            asio::post(*async_io_context, [handler = std::forward<CompletionHandler>(handler),
+                                           error = url_result.error()] mutable { handler(std::unexpected(error)); });
             return;
          }
 
@@ -263,9 +273,8 @@ namespace glz
       {
          auto url_result = parse_url(url);
          if (!url_result) {
-            asio::post(*io_context, [handler = std::forward<CompletionHandler>(handler), error = url_result.error()] mutable {
-               handler(std::unexpected(error));
-            });
+            asio::post(*async_io_context, [handler = std::forward<CompletionHandler>(handler),
+                                           error = url_result.error()] mutable { handler(std::unexpected(error)); });
             return;
          }
 
@@ -288,23 +297,6 @@ namespace glz
          return future;
       }
 
-      // JSON POST request
-      template <class T>
-      std::expected<response, std::error_code> post_json(
-         std::string_view url, const T& data, const std::unordered_map<std::string, std::string>& headers = {})
-      {
-         std::string json_str;
-         auto ec = glz::write_json(data, json_str);
-         if (ec) {
-            return std::unexpected(std::make_error_code(std::errc::invalid_argument));
-         }
-
-         auto merged_headers = headers;
-         merged_headers["Content-Type"] = "application/json";
-
-         return post(url, json_str, merged_headers);
-      }
-
       // Async JSON POST request
       template <class T, typename CompletionHandler>
       void post_json_async(std::string_view url, const T& data,
@@ -313,7 +305,7 @@ namespace glz
          std::string json_str;
          auto ec = glz::write_json(data, json_str);
          if (ec) {
-            asio::post(*io_context, [handler = std::forward<CompletionHandler>(handler)]() {
+            asio::post(*async_io_context, [handler = std::forward<CompletionHandler>(handler)]() {
                handler(std::unexpected(std::make_error_code(std::errc::invalid_argument)));
             });
             return;
@@ -342,7 +334,7 @@ namespace glz
       }
 
      private:
-      std::shared_ptr<asio::io_context> io_context;
+      std::shared_ptr<asio::io_context> async_io_context; // For async operations only
       std::shared_ptr<http_connection_pool> connection_pool;
       std::vector<std::thread> worker_threads;
       std::atomic<bool> running{true};
@@ -356,7 +348,7 @@ namespace glz
             worker_threads.emplace_back([this]() {
                while (running) {
                   try {
-                     io_context->run();
+                     async_io_context->run();
                      break; // Normal exit
                   }
                   catch (const std::exception& e) {
@@ -371,12 +363,123 @@ namespace glz
       void stop_workers()
       {
          running = false;
-         io_context->stop();
+         async_io_context->stop();
 
          for (auto& thread : worker_threads) {
             if (thread.joinable()) {
                thread.join();
             }
+         }
+      }
+
+      std::expected<response, std::error_code> perform_sync_request(
+         const std::string& method, const url_parts& url, const std::string& body,
+         const std::unordered_map<std::string, std::string>& headers)
+      {
+         auto socket = connection_pool->get_connection(url.host, url.port);
+
+         try {
+            // If socket is not connected, connect it synchronously
+            if (!socket->is_open()) {
+               asio::ip::tcp::resolver resolver(*async_io_context);
+               auto endpoints = resolver.resolve(url.host, std::to_string(url.port));
+               asio::connect(*socket, endpoints);
+            }
+
+            // Build HTTP request
+            std::string request_str;
+            request_str.append(method);
+            request_str.append(" ");
+            request_str.append(url.path);
+            request_str.append(" HTTP/1.1\r\n");
+            request_str.append("Host: ");
+            request_str.append(url.host);
+            request_str.append("\r\n");
+            request_str.append("Connection: keep-alive\r\n"); // Keep connection alive for reuse
+
+            if (!body.empty()) {
+               request_str.append("Content-Length: ");
+               request_str.append(std::to_string(body.size()));
+               request_str.append("\r\n");
+            }
+
+            for (const auto& [name, value] : headers) {
+               request_str.append(name);
+               request_str.append(": ");
+               request_str.append(value);
+               request_str.append("\r\n");
+            }
+
+            request_str.append("\r\n");
+            request_str.append(body);
+
+            // Send request synchronously
+            asio::write(*socket, asio::buffer(request_str));
+
+            // Read response headers synchronously
+            asio::streambuf response_buffer;
+            asio::read_until(*socket, response_buffer, "\r\n\r\n");
+
+            // Parse status line
+            std::istream response_stream(&response_buffer);
+            std::string status_line;
+            std::getline(response_stream, status_line);
+
+            if (!status_line.empty() && status_line.back() == '\r') {
+               status_line.pop_back();
+            }
+
+            auto parsed_status = parse_http_status_line(status_line);
+            if (!parsed_status) {
+               return std::unexpected(parsed_status.error());
+            }
+
+            // Parse headers
+            std::unordered_map<std::string, std::string> response_headers;
+            std::string header_line;
+            while (std::getline(response_stream, header_line) && header_line != "\r") {
+               if (header_line.back() == '\r') {
+                  header_line.pop_back();
+               }
+
+               auto colon_pos = header_line.find(':');
+               if (colon_pos != std::string::npos) {
+                  std::string name = header_line.substr(0, colon_pos);
+                  std::string value = header_line.substr(colon_pos + 1);
+                  value.erase(0, value.find_first_not_of(" \t"));
+                  response_headers[name] = value;
+               }
+            }
+
+            // Read response body synchronously
+            std::error_code ec;
+            asio::read(*socket, response_buffer, asio::transfer_all(), ec);
+            // EOF is expected for some responses
+            if (ec && ec != asio::error::eof) {
+               return std::unexpected(ec);
+            }
+
+            std::string response_body{std::istreambuf_iterator<char>(&response_buffer),
+                                      std::istreambuf_iterator<char>()};
+
+            response resp;
+            resp.status_code = parsed_status->status_code;
+            resp.response_headers = std::move(response_headers);
+            resp.response_body = std::move(response_body);
+
+            // Return connection to pool if it's still usable
+            auto connection_header = resp.response_headers.find("Connection");
+            if (connection_header == resp.response_headers.end() || connection_header->second != "close") {
+               connection_pool->return_connection(url.host, url.port, socket);
+            }
+
+            return resp;
+         }
+         catch (const std::system_error& e) {
+            return std::unexpected(e.code());
+         }
+         catch (...) {
+            return std::unexpected(std::make_error_code(std::errc::connection_refused));
          }
       }
 
@@ -386,7 +489,7 @@ namespace glz
                                  CompletionHandler&& handler)
       {
          auto socket = connection_pool->get_connection(url.host, url.port);
-         auto resolver = std::make_shared<asio::ip::tcp::resolver>(*io_context);
+         auto resolver = std::make_shared<asio::ip::tcp::resolver>(*async_io_context);
 
          resolver->async_resolve(
             url.host, std::to_string(url.port),
@@ -459,7 +562,6 @@ namespace glz
          request_str.append(body);
 
          // Use shared_ptr to keep request string alive during async operation
-         // We can't use move capture here because asio::buffer() needs the string before lambda creation
          auto request_str_ptr = std::make_shared<std::string>(std::move(request_str));
          asio::async_write(*socket, asio::buffer(*request_str_ptr),
                            [this, socket, request_str_ptr, url, handler = std::forward<CompletionHandler>(handler)](
