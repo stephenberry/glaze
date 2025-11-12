9e8246fc14189af26e6fcb2a4e20936762136bf8
stephenberry <stephenberry@users.noreply.github.com>
2025-11-12 01:02:45 +0000

    Committing clang-format changes

--- include/glaze/core/opts.hpp
+++ include/glaze/core/opts.hpp
@@ -183,8 +183,7 @@ namespace glz
    // The maximum precision type used for writing floats, higher precision floats will be cast down to this precision
 
    struct bools_as_numbers_opt_tag
-   {
-   };
+   {};
 
    template <auto member_ptr>
    concept is_bools_as_numbers_tag = std::same_as<std::decay_t<decltype(member_ptr)>, bools_as_numbers_opt_tag>;
