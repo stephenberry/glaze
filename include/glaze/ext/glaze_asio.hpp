df78bf365da822ad0f8e6b014d198807cab9fe81
stephenberry <stephenberry@users.noreply.github.com>
2025-11-27 01:36:20 +0000

    Committing clang-format changes

--- include/glaze/ext/glaze_asio.hpp
+++ include/glaze/ext/glaze_asio.hpp
@@ -20,8 +20,8 @@ static_assert(false, "standalone or boost asio must be included to use glaze/ext
 #include <coroutine>
 #include <iostream>
 
-#include "glaze/rpc/repe/buffer.hpp"
 #include "glaze/rpc/registry.hpp"
+#include "glaze/rpc/repe/buffer.hpp"
 #include "glaze/util/memory_pool.hpp"
 
 namespace glz
