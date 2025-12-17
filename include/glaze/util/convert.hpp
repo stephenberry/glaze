e9dfbb67a8e5be01e3064c3ef3261e95d81b90bc
Stephen Berry <stephenberry.developer@gmail.com>
2025-12-17 05:37:21 -0600

    fix to_uint16_t for big endian

--- include/glaze/util/convert.hpp
+++ include/glaze/util/convert.hpp
@@ -3,9 +3,19 @@
 
 #pragma once
 
+#include <bit>
 #include <cstdint>
 
 namespace glz
 {
-   consteval uint16_t to_uint16_t(const char chars[2]) { return uint16_t(chars[0]) | (uint16_t(chars[1]) << 8); }
+   // Creates a uint16_t from two chars that matches memcpy behavior on any endianness
+   consteval uint16_t to_uint16_t(const char chars[2])
+   {
+      if constexpr (std::endian::native == std::endian::little) {
+         return uint16_t(chars[0]) | (uint16_t(chars[1]) << 8);
+      }
+      else {
+         return (uint16_t(chars[0]) << 8) | uint16_t(chars[1]);
+      }
+   }
 }
