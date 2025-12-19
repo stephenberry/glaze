af5e09c96656f50d47b27f1652faeb31c529cff5
Stephen Berry <stephenberry.developer@gmail.com>
2025-12-19 11:20:10 -0600

    update schema tests with default outputs

--- tests/example_json/example_json.cpp
+++ tests/example_json/example_json.cpp
@@ -471,7 +471,7 @@ suite schema_generation = [] {
       auto schema = glz::write_json_schema<SchemaDemo>().value_or("error");
       expect(
          schema ==
-         R"({"type":["object"],"properties":{"flag":{"$ref":"#/$defs/bool","description":"A boolean flag"},"name":{"$ref":"#/$defs/std::string","description":"A name for something"},"x":{"$ref":"#/$defs/int32_t","description":"An integer x"}},"additionalProperties":false,"$defs":{"bool":{"type":["boolean"]},"int32_t":{"type":["integer"],"minimum":-2147483648,"maximum":2147483647},"std::string":{"type":["string"]}},"title":"SchemaDemo"})")
+         R"({"type":["object"],"properties":{"flag":{"$ref":"#/$defs/bool","description":"A boolean flag","default":false},"name":{"$ref":"#/$defs/std::string","description":"A name for something"},"x":{"$ref":"#/$defs/int32_t","description":"An integer x","default":0}},"additionalProperties":false,"$defs":{"bool":{"type":["boolean"]},"int32_t":{"type":["integer"],"minimum":-2147483648,"maximum":2147483647},"std::string":{"type":["string"]}},"title":"SchemaDemo"})")
          << schema;
    };
 };
@@ -500,7 +500,7 @@ suite local_schema_test = [] {
       auto schema = glz::write_json_schema<LocalSchema>().value_or("error");
       expect(
          schema ==
-         R"({"type":["object"],"properties":{"count":{"$ref":"#/$defs/int32_t","description":"A count"},"file":{"$ref":"#/$defs/std::string","description":"A file path"},"valid":{"$ref":"#/$defs/bool","description":"Validity flag"}},"additionalProperties":false,"$defs":{"bool":{"type":["boolean"]},"int32_t":{"type":["integer"],"minimum":-2147483648,"maximum":2147483647},"std::string":{"type":["string"]}},"title":"LocalSchema"})")
+         R"({"type":["object"],"properties":{"count":{"$ref":"#/$defs/int32_t","description":"A count","default":0},"file":{"$ref":"#/$defs/std::string","description":"A file path"},"valid":{"$ref":"#/$defs/bool","description":"Validity flag","default":false}},"additionalProperties":false,"$defs":{"bool":{"type":["boolean"]},"int32_t":{"type":["integer"],"minimum":-2147483648,"maximum":2147483647},"std::string":{"type":["string"]}},"title":"LocalSchema"})")
          << schema;
    };
 };
