705c5bdd100ceb88a9d55180a23f114e06af7ac6
Stephen Berry <stephenberry.developer@gmail.com>
2026-04-16 14:16:49 -0500

    JSON Schema: Exempt numeric types from single-use inlining (#2505)

--- tests/example_json/example_json.cpp
+++ tests/example_json/example_json.cpp
@@ -471,7 +471,7 @@ suite schema_generation = [] {
       auto schema = glz::write_json_schema<SchemaDemo>().value_or("error");
       expect(
          schema ==
-         R"({"type":"object","properties":{"flag":{"type":"boolean","description":"A boolean flag"},"name":{"type":"string","description":"A name for something"},"x":{"type":"integer","description":"An integer x","minimum":-2147483648,"maximum":2147483647}},"additionalProperties":false,"title":"SchemaDemo"})")
+         R"({"type":"object","properties":{"flag":{"type":"boolean","description":"A boolean flag"},"name":{"type":"string","description":"A name for something"},"x":{"$ref":"#/$defs/int32_t","description":"An integer x"}},"additionalProperties":false,"$defs":{"int32_t":{"type":"integer","minimum":-2147483648,"maximum":2147483647}},"title":"SchemaDemo"})")
          << schema;
    };
 };
@@ -500,7 +500,7 @@ suite local_schema_test = [] {
       auto schema = glz::write_json_schema<LocalSchema>().value_or("error");
       expect(
          schema ==
-         R"({"type":"object","properties":{"count":{"type":"integer","description":"A count","minimum":-2147483648,"maximum":2147483647},"file":{"type":"string","description":"A file path"},"valid":{"type":"boolean","description":"Validity flag"}},"additionalProperties":false,"title":"LocalSchema"})")
+         R"({"type":"object","properties":{"count":{"$ref":"#/$defs/int32_t","description":"A count"},"file":{"type":"string","description":"A file path"},"valid":{"type":"boolean","description":"Validity flag"}},"additionalProperties":false,"$defs":{"int32_t":{"type":"integer","minimum":-2147483648,"maximum":2147483647}},"title":"LocalSchema"})")
          << schema;
    };
 };
