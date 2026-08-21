9955616596538c8c027fda9e20ff9b1089ec44a5
Makai Mann <makaim@stanford.edu>
2026-08-20 22:22:02 -0400

    Add failing test for off-by-one

--- tests/json_reflection_test/json_reflection_test.cpp
+++ tests/json_reflection_test/json_reflection_test.cpp
@@ -1416,6 +1416,19 @@ struct elision_outer
 static_assert(glz::detail::count_members<elision_outer> == 2,
               "brace elision must not overcount");
 
+struct max_members_t
+{
+   #define FIELDS_8(p) int p##0, p##1, p##2, p##3, p##4, p##5, p##6, p##7;
+   #define FIELDS_64(p) FIELDS_8(p##a) FIELDS_8(p##b) FIELDS_8(p##c) FIELDS_8(p##d) FIELDS_8(p##e) FIELDS_8(p##f) FIELDS_8(p##g) FIELDS_8(p##h)
+
+   FIELDS_64(x) FIELDS_64(y)
+
+   #undef FIELDS_64
+   #undef FIELD_8
+}
+static_assert(glz::detail::count_members<max_members_t> == glz::detail::max_pure_reflection_count,
+              "a struct with exactly max_pure_reflection_count members must not count one short");
+
 suite non_default_constructible_member_tests = [] {
    "non-default-constructible member at position 2"_test = [] {
       std::string buffer{};
