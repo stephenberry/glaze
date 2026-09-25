57d78dd5d330db6b75367d303970ffd2e46106e7
Vincent <60389548+vsmcea@users.noreply.github.com>
2026-09-25 18:00:57 +0200

    Refactor bitmask initialization using array (#2946)
    * Refactor bitmask initialization using array
    
    * Make bits_array static
    
    ---------
    
    Co-authored-by: Stephen Berry <stephenberry.developer@gmail.com>

--- include/glaze/simd/structural.hpp
+++ include/glaze/simd/structural.hpp
@@ -114,8 +114,9 @@ namespace glz::detail::structural
    GLZ_ALWAYS_INLINE uint64_t to_bitmask(const uint8x16_t a, const uint8x16_t b, const uint8x16_t c,
                                          const uint8x16_t d) noexcept
    {
-      const uint8x16_t bits = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
-                               0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
+      static constexpr uint8_t bits_array[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
+                                               0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
+      const uint8x16_t bits = vld1q_u8(bits_array);
       uint8x16_t s0 = vpaddq_u8(vandq_u8(a, bits), vandq_u8(b, bits));
       const uint8x16_t s1 = vpaddq_u8(vandq_u8(c, bits), vandq_u8(d, bits));
       s0 = vpaddq_u8(s0, s1);
